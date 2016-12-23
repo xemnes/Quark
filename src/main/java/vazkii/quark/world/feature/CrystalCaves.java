d4d224d441d0d8144e04d4868a8932e7e3de9afd
Vasco Lavos <vazkii@hotmail.com>
2016-12-23 19:11:53 +0000

    Crystals can now be crafted into runes if runes are enabled

--- src/main/java/vazkii/quark/world/feature/CrystalCaves.java
+++ src/main/java/vazkii/quark/world/feature/CrystalCaves.java
@@ -1,10 +1,14 @@
 package vazkii.quark.world.feature;
 
 import net.minecraft.block.Block;
-import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import vazkii.arl.util.RecipeHandler;
 import vazkii.quark.base.module.Feature;
+import vazkii.quark.base.module.ModuleLoader;
+import vazkii.quark.content.feature.ColorRunes;
 import vazkii.quark.world.block.BlockCrystal;
 import vazkii.quark.world.world.CrystalCaveGenerator;
 
@@ -24,6 +28,27 @@ public class CrystalCaves extends Feature {
 		crystal = new BlockCrystal();
 		
 		GameRegistry.registerWorldGenerator(new CrystalCaveGenerator(), 1);
+	}
+	
+	@Override
+	public void init(FMLInitializationEvent event) {
+		if(ModuleLoader.isFeatureEnabled(ColorRunes.class)) {
+			addRuneRecipe(0, 0);
+			addRuneRecipe(1, 14);
+			addRuneRecipe(2, 1);
+			addRuneRecipe(3, 4);
+			addRuneRecipe(4, 5);
+			addRuneRecipe(5, 3);
+			addRuneRecipe(6, 11);
+			addRuneRecipe(7, 2);
+		}
+	}
+
+	private void addRuneRecipe(int crystalMeta, int runeMeta) {
+		RecipeHandler.addOreDictRecipe(new ItemStack(ColorRunes.rune, 1, runeMeta), 
+				"CCC", "CSC", "CCC",
+				'C', new ItemStack(crystal, 1, crystalMeta),
+				'S', "stone");
 	}
 	
 }
