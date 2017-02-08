893a16b2ff737384e508b7e111f2310f93e3c78e
Vasco Lavos <vazkii@hotmail.com>
2017-02-08 23:52:53 +0000

    Stained Planks

---
+++ src/main/java/vazkii/quark/building/block/BlockVerticalStainedPlanks.java
@@ -0,0 +1,38 @@
+package vazkii.quark.building.block;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import vazkii.arl.block.BlockMetaVariants;
+import vazkii.quark.base.block.IQuarkBlock;
+
+public class BlockVerticalStainedPlanks extends BlockMetaVariants implements IQuarkBlock {
+
+	public BlockVerticalStainedPlanks() {
+		super("vertical_stained_planks", Material.ROCK, Variants.class);
+		setHardness(2.0F);
+		setResistance(5.0F);
+		setSoundType(SoundType.WOOD);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+	}
+
+	public static enum Variants implements EnumBase {
+		VERTICAL_STAINED_PLANKS_WHITE,
+		VERTICAL_STAINED_PLANKS_ORANGE,
+		VERTICAL_STAINED_PLANKS_MAGENTA,
+		VERTICAL_STAINED_PLANKS_LIGHT_BLUE,
+		VERTICAL_STAINED_PLANKS_YELLOW,
+		VERTICAL_STAINED_PLANKS_LIME,
+		VERTICAL_STAINED_PLANKS_PINK,
+		VERTICAL_STAINED_PLANKS_GRAY,
+		VERTICAL_STAINED_PLANKS_SILVER,
+		VERTICAL_STAINED_PLANKS_CYAN,
+		VERTICAL_STAINED_PLANKS_PURPLE,
+		VERTICAL_STAINED_PLANKS_BLUE,
+		VERTICAL_STAINED_PLANKS_BROWN,
+		VERTICAL_STAINED_PLANKS_GREEN,
+		VERTICAL_STAINED_PLANKS_RED,
+		VERTICAL_STAINED_PLANKS_BLACK
+	}
+
+}
---
+++ src/main/java/vazkii/quark/building/feature/StainedPlanks.java
@@ -0,0 +1,54 @@
+package vazkii.quark.building.feature;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.oredict.OreDictionary;
+import vazkii.arl.block.BlockMod;
+import vazkii.arl.block.BlockModSlab;
+import vazkii.arl.block.BlockModStairs;
+import vazkii.arl.util.RecipeHandler;
+import vazkii.quark.base.lib.LibMisc;
+import vazkii.quark.base.module.Feature;
+import vazkii.quark.building.block.BlockStainedPlanks;
+import vazkii.quark.building.block.slab.BlockStainedPlanksSlab;
+import vazkii.quark.building.block.stairs.BlockStainedPlanksStairs;
+
+public class StainedPlanks extends Feature {
+
+	public static BlockMod stained_planks;
+
+	boolean enableStairsAndSlabs;
+
+	@Override
+	public void setupConfig() {
+		enableStairsAndSlabs = loadPropBool("Enable stairs and slabs", "", true);
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		stained_planks = new BlockStainedPlanks();
+
+		if(enableStairsAndSlabs) {
+			for(BlockStainedPlanks.Variants variant : BlockStainedPlanks.Variants.class.getEnumConstants())
+				BlockModStairs.initStairs(stained_planks, variant.ordinal(), new BlockStainedPlanksStairs(variant));
+			for(BlockStainedPlanks.Variants variant : BlockStainedPlanks.Variants.class.getEnumConstants())
+				BlockModSlab.initSlab(stained_planks, variant.ordinal(), new BlockStainedPlanksSlab(variant, false), new BlockStainedPlanksSlab(variant, true));
+		}
+
+		OreDictionary.registerOre("plankWood", new ItemStack(stained_planks, 1, OreDictionary.WILDCARD_VALUE));
+		OreDictionary.registerOre("plankStained", new ItemStack(stained_planks, 1, OreDictionary.WILDCARD_VALUE));
+		
+		for(int i = 0; i < 16; i++) {
+			RecipeHandler.addOreDictRecipe(new ItemStack(stained_planks, 8, i),
+					"BBB", "BDB", "BBB",
+					'B', "plankWood",
+					'D', LibMisc.OREDICT_DYES.get(15 - i));
+		}
+	}
+
+	@Override
+	public boolean requiresMinecraftRestartToEnable() {
+		return true;
+	}
+
+}
