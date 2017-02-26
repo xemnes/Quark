83bbd3642ba694c8c1932dde85f10524fc2ae398
Vasco Lavos <vazkii@hotmail.com>
2017-02-26 00:54:31 +0000

    Move global size configs and hardcoded heights to biome configs

--- src/main/java/vazkii/quark/world/feature/UndergroundBiomes.java
+++ src/main/java/vazkii/quark/world/feature/UndergroundBiomes.java
@@ -23,21 +23,10 @@ public class UndergroundBiomes extends Feature {
 
 	public List<UndergroundBiomeInfo> biomes;
 	
-	int minXSize, minYSize, minZSize;
-	int xVariation, yVariation, zVariation;
-	
 	@Override
 	public void setupConfig() {
 		biomes = new ArrayList();
 		biomes.add(loadUndergrondBiomeInfo("Lush", new UndergroundBiomeLush(), 50, Type.JUNGLE));
-		
-		minXSize = loadPropInt("Min X Size", "", 26);
-		minYSize = loadPropInt("Min Y Size", "", 12);
-		minZSize = loadPropInt("Min Z Size", "", 26);
-		
-		xVariation = loadPropInt("X Axis Variation", "", 14);
-		yVariation = loadPropInt("Y Axis Variation", "", 6);
-		zVariation = loadPropInt("Z Axis Variation", "", 14);
 	}
 	
 	@SubscribeEvent
@@ -47,14 +36,14 @@ public class UndergroundBiomes extends Feature {
 			BlockPos pos = event.getPos();
 			Random rand = event.getRand();
 			
-			BlockPos spawnPos = pos.add(rand.nextInt(16), rand.nextInt(world.getSeaLevel()), rand.nextInt(16));
-			Biome biome = world.getBiome(spawnPos);
-
 			for(UndergroundBiomeInfo biomeInfo : biomes) {
+				BlockPos spawnPos = pos.add(rand.nextInt(16), biomeInfo.minY + rand.nextInt(biomeInfo.maxY - biomeInfo.minY), rand.nextInt(16));
+				Biome biome = world.getBiome(spawnPos);
+				
 				if(BiomeTypeConfigHandler.biomeTypeIntersectCheck(biomeInfo.types, biome) && rand.nextInt(biomeInfo.rarity) == 0) {
-					int radiusX = minXSize + rand.nextInt(xVariation);
-					int radiusY = minYSize + rand.nextInt(yVariation);
-					int radiusZ = minZSize + rand.nextInt(zVariation);
+					int radiusX = biomeInfo.minXSize + rand.nextInt(biomeInfo.xVariation);
+					int radiusY = biomeInfo.minYSize + rand.nextInt(biomeInfo.yVariation);
+					int radiusZ = biomeInfo.minZSize + rand.nextInt(biomeInfo.zVariation);
 					
 					biomeInfo.biome.apply(world, spawnPos, radiusX, radiusY, radiusZ);
 					return;
@@ -86,6 +75,9 @@ public class UndergroundBiomes extends Feature {
 		public final UndergroundBiome biome;
 		public final List<BiomeDictionary.Type> types;
 		public final int rarity;
+		int minXSize, minYSize, minZSize;
+		int xVariation, yVariation, zVariation;
+		int minY, maxY;
 		
 		private UndergroundBiomeInfo(String category, UndergroundBiome biome, int rarity, BiomeDictionary.Type... biomes) {
 			this.enabled = ModuleLoader.config.getBoolean("Enabled", category, true, "");
@@ -93,7 +85,20 @@ public class UndergroundBiomes extends Feature {
 			this.types = BiomeTypeConfigHandler.parseBiomeTypeArrayConfig("Allowed Biome Types", category, biomes);
 			this.rarity = ModuleLoader.config.getInt("Rarity", category, rarity, 0, Integer.MAX_VALUE, "This biome will spawn in 1 of X valid chunks");
 			
-			biome.setupConfig(category);
+			minY = ModuleLoader.config.getInt("Minimum Y Level", category, 10, 0, Integer.MAX_VALUE, "");
+			maxY = ModuleLoader.config.getInt("Maximum Y Level", category, 40, 0, Integer.MAX_VALUE, "");
+			
+			String sizeCategory = category + ".size";
+			
+			minXSize = ModuleLoader.config.getInt("X Minimum", sizeCategory, 26, 0, Integer.MAX_VALUE, "");
+			minYSize = ModuleLoader.config.getInt("Y Minimum", sizeCategory, 12, 0, Integer.MAX_VALUE, "");
+			minZSize = ModuleLoader.config.getInt("Z Minimum", sizeCategory, 26, 0, Integer.MAX_VALUE, "");
+			
+			xVariation = ModuleLoader.config.getInt("X Variation", sizeCategory, 14, 0, Integer.MAX_VALUE, "");
+			yVariation = ModuleLoader.config.getInt("Y Variation", sizeCategory, 6, 0, Integer.MAX_VALUE, "");
+			zVariation = ModuleLoader.config.getInt("Z Variation", sizeCategory, 14, 0, Integer.MAX_VALUE, "");
+			
+			biome.setupConfig(category + ".specific");
 		}
 
 		
