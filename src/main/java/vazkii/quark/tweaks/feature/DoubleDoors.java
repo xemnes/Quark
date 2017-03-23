8493446512845b6ee0e3516bbf52559f391255e3
Vasco Lavos <vazkii@hotmail.com>
2017-03-23 22:24:41 +0000

    Fix the delay when using double doors

--- src/main/java/vazkii/quark/tweaks/feature/DoubleDoors.java
+++ src/main/java/vazkii/quark/tweaks/feature/DoubleDoors.java
@@ -12,9 +12,11 @@ package vazkii.quark.tweaks.feature;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDoor;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -25,10 +27,11 @@ public class DoubleDoors extends Feature {
 
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public void onPlayerInteract(PlayerInteractEvent.RightClickBlock event) {
-		if(event.getEntityPlayer().isSneaking() || event.getWorld().isRemote || event.isCanceled() || event.getResult() == Result.DENY)
+		if(event.getEntityPlayer().isSneaking() || event.isCanceled() || event.getResult() == Result.DENY)
 			return;
 
-		IBlockState state = event.getWorld().getBlockState(event.getPos()).getActualState(event.getWorld(), event.getPos());
+		World world = event.getWorld();
+		IBlockState state = world.getBlockState(event.getPos()).getActualState(world, event.getPos());
 		Block block = state.getBlock();
 
 		if(!(block instanceof BlockDoor))
@@ -38,18 +41,22 @@ public class DoubleDoors extends Feature {
 		boolean isOpen = state.getValue(BlockDoor.OPEN);
 		BlockDoor.EnumHingePosition isMirrored = state.getValue(BlockDoor.HINGE);
 
-		BlockPos pos = event.getPos().offset(isMirrored == BlockDoor.EnumHingePosition.RIGHT ? direction.rotateYCCW() : direction.rotateY());
-		IBlockState other = event.getWorld().getBlockState(pos).getActualState(event.getWorld(), pos);
+		BlockPos mirrorPos = event.getPos().offset(isMirrored == BlockDoor.EnumHingePosition.RIGHT ? direction.rotateYCCW() : direction.rotateY());
+		BlockPos doorPos = state.getValue(BlockDoor.HALF) == BlockDoor.EnumDoorHalf.LOWER ? mirrorPos : mirrorPos.down();
+		IBlockState other = world.getBlockState(doorPos).getActualState(world, doorPos);
 
-		if(other.getBlock() == (BlockDoor) block && other.getValue(BlockDoor.FACING) == direction && other.getValue(BlockDoor.OPEN) == isOpen && other.getValue(BlockDoor.HINGE) != isMirrored)
-			((BlockDoor) block).onBlockActivated(event.getWorld(), pos, other, event.getEntityPlayer(), event.getHand(), event.getFace(), 0, 0, 0);
+		if(block.getMaterial(state) != Material.IRON && other.getBlock() == (BlockDoor) block && other.getValue(BlockDoor.FACING) == direction && other.getValue(BlockDoor.OPEN) == isOpen && other.getValue(BlockDoor.HINGE) != isMirrored) {
+
+			IBlockState newState = other.cycleProperty(BlockDoor.OPEN);
+			world.setBlockState(doorPos, newState, 10);
+		}
 	}
 
 	@Override
 	public boolean hasSubscriptions() {
 		return true;
 	}
-	
+
 	@Override
 	public String[] getIncompatibleMods() {
 		return new String[] { "malisisdoors", "CharsetTweaks" };
