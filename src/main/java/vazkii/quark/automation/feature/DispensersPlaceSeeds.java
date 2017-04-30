9781f62dab0d6f2d26c1171c88f48ddd5dd1d0b9
Vasco Lavos <vazkii@hotmail.com>
2017-04-30 20:15:37 +0100

    Added a config to add custom seeds to dispensers. Closes #414

--- src/main/java/vazkii/quark/automation/feature/DispensersPlaceSeeds.java
+++ src/main/java/vazkii/quark/automation/feature/DispensersPlaceSeeds.java
@@ -10,6 +10,9 @@
  */
 package vazkii.quark.automation.feature;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDispenser;
 import net.minecraft.block.BlockHorizontal;
@@ -28,10 +31,43 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import vazkii.quark.base.module.ConfigHelper;
 import vazkii.quark.base.module.Feature;
 
 public class DispensersPlaceSeeds extends Feature {
 
+	Map<Item, IBlockState> customSeeds;
+	
+	@Override
+	public void setupConfig() {
+		ConfigHelper.needsRestart = true;
+		String[] customSeedsArr = loadPropStringList("Custom Seeds", "Add seeds from other mods here, in the following format: mod:seed=mod:block:meta. Set meta to -1 to just place the default.", new String[0]);
+		
+		customSeeds = new HashMap();
+		for(String s : customSeedsArr) {
+			String[] tokens = s.split("=");
+			if(tokens.length == 2) {
+				String key = tokens[0];
+				String value = tokens[1];
+				Item item = Item.getByNameOrId(key);
+				if(item != null) {
+					tokens = value.split(":");
+					if(tokens.length == 3)
+						try {
+							value = tokens[0] + ":" + tokens[1];
+							int meta = Integer.parseInt(tokens[2]);
+							Block block = Block.getBlockFromName(value);
+							if(block != null) {
+								if(meta == -1)
+									customSeeds.put(item, block.getDefaultState());
+								else customSeeds.put(item, block.getStateFromMeta(meta));
+							}
+						} catch(NumberFormatException e) { }
+				}
+			}
+		}
+	}
+	
 	@Override
 	public void init(FMLInitializationEvent event) {
 		BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.WHEAT_SEEDS, new BehaviourSeeds(Blocks.WHEAT));
@@ -44,6 +80,9 @@ public class DispensersPlaceSeeds extends Feature {
 		BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Item.getItemFromBlock(Blocks.CHORUS_FLOWER), new BehaviourSeeds(Blocks.CHORUS_FLOWER));
 
 		BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.DYE, new BehaviourCocoaBeans(BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.getObject(Items.DYE)));
+		
+		for(Item i : customSeeds.keySet())
+			BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(i, new BehaviourSeeds(customSeeds.get(i)));
 	}
 	
 	@Override
@@ -53,10 +92,14 @@ public class DispensersPlaceSeeds extends Feature {
 
 	public class BehaviourSeeds extends BehaviorDefaultDispenseItem {
 
-		Block block;
+		private final IBlockState placeState;
 
 		public BehaviourSeeds(Block block) {
-			this.block = block;
+			this(block.getDefaultState());
+		}
+		
+		public BehaviourSeeds(IBlockState state) {
+			placeState = state;
 		}
 
 		@Override
@@ -65,8 +108,8 @@ public class DispensersPlaceSeeds extends Feature {
 			BlockPos pos = par1IBlockSource.getBlockPos().offset(facing);
 			World world = par1IBlockSource.getWorld();
 
-			if(world.isAirBlock(pos) && block.canPlaceBlockAt(world, pos)) {
-				world.setBlockState(pos, block.getDefaultState());
+			if(world.isAirBlock(pos) && placeState.getBlock().canPlaceBlockAt(world, pos)) {
+				world.setBlockState(pos, placeState);
 				par2ItemStack.shrink(1);
 				return par2ItemStack;
 			}
