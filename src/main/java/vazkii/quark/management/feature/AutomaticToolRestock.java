85a8cbaf4fd1a31e76d01055c3f20bb6aeb4d465
Vasco Lavos <vazkii@hotmail.com>
2017-06-29 17:47:40 +0100

    Delay auto restock by one tick to prevent item destruction and allow it to work with every item in the game. Closes #479

--- src/main/java/vazkii/quark/management/feature/AutomaticToolRestock.java
+++ src/main/java/vazkii/quark/management/feature/AutomaticToolRestock.java
@@ -5,7 +5,11 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
+import java.util.WeakHashMap;
 import java.util.function.Predicate;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -23,20 +27,26 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import vazkii.quark.base.module.Feature;
 
 public class AutomaticToolRestock extends Feature {
 
+	private static final WeakHashMap<EntityPlayer, Stack<Pair<Integer, Integer>>> replacements = new WeakHashMap();
+	
 	public List<Enchantment> importantEnchants = new ArrayList();
 	private String[] enchantNames;
 	private boolean enableLooseMatching;
 	private boolean enableEnchantMatching;
+	private boolean unstackablesOnly;
 
 	@Override
 	public void setupConfig() {
 		enchantNames = loadPropStringList("Important Enchantments", "Enchantments deemed important enough to have special priority when finding a replacement", generateDefaultEnchantmentList());
 		enableLooseMatching = loadPropBool("Enable Loose Matching", "Enable replacing your tools with tools of the same type but not the same item", true);
 		enableEnchantMatching = loadPropBool("Enable Enchantment Matching", "Enable comparing enchantments to find a replacement", true);
+		unstackablesOnly = loadPropBool("Unstackable Items Only", "", false);
 	}
 
 	@Override
@@ -56,13 +66,16 @@ public class AutomaticToolRestock extends Feature {
 		ItemStack stack = event.getOriginal();
 		Item item = stack.getItem();
 
-		if(!player.world.isRemote && !stack.isEmpty() && stack.isItemStackDamageable() && stack.getMaxStackSize() == 1 && !(item instanceof ItemArmor)) {
+		if(!player.world.isRemote && !stack.isEmpty() && !(item instanceof ItemArmor) && (!unstackablesOnly || !stack.isStackable())) {
 			int currSlot = player.inventory.currentItem;
 			if(event.getHand() == EnumHand.OFF_HAND)
 				currSlot = player.inventory.getSizeInventory() - 1;
 
 			List<Enchantment> enchantmentsOnStack = getImportantEnchantments(stack);
 			Predicate<ItemStack> itemPredicate = (other) -> other.getItem() == item;
+			if(!stack.isItemStackDamageable())
+				itemPredicate = itemPredicate.and((other) -> other.getItemDamage() == stack.getItemDamage());
+			
 			Predicate<ItemStack> enchantmentPredicate = (other) -> !(new ArrayList(enchantmentsOnStack)).retainAll(getImportantEnchantments(other));
 
 			if(enableEnchantMatching && findReplacement(player, currSlot, itemPredicate.and(enchantmentPredicate)))
@@ -82,6 +95,17 @@ public class AutomaticToolRestock extends Feature {
 
 					findReplacement(player, currSlot, toolPredicate);
 				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onPlayerTick(PlayerTickEvent event) {
+		if(event.phase == Phase.END && replacements.containsKey(event.player)) {
+			Stack<Pair<Integer, Integer>> replacementStack = replacements.get(event.player);
+			while(!replacementStack.isEmpty()) {
+				Pair<Integer, Integer> pair = replacementStack.pop();
+				switchItems(event.player, pair.getLeft(), pair.getRight());
 			}
 		}
 	}
@@ -107,12 +131,18 @@ public class AutomaticToolRestock extends Feature {
 
 			ItemStack stackAt = player.inventory.getStackInSlot(i);
 			if(match.test(stackAt)) {
-				switchItems(player, i, currSlot);
+				pushReplace(player, i, currSlot);
 				return true;
 			}
 		}
 
 		return false;
+	}
+	
+	private void pushReplace(EntityPlayer player, int slot1, int slot2) {
+		if(!replacements.containsKey(player))
+			replacements.put(player, new Stack());
+		replacements.get(player).push(Pair.of(slot1, slot2));
 	}
 
 	private void switchItems(EntityPlayer player, int slot1, int slot2) {
