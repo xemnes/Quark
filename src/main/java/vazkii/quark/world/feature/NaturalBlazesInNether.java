77bf9d5730e47a30bc2c8b098d8b0aab1bc9b004
Vasco Lavos <vazkii@hotmail.com>
2017-07-02 23:53:01 +0100

    Lowered the spawn rate of natural blaze spawns and made them not spawn outside of netherrack to not ruin your builds

--- src/main/java/vazkii/quark/world/feature/NaturalBlazesInNether.java
+++ src/main/java/vazkii/quark/world/feature/NaturalBlazesInNether.java
@@ -13,19 +13,27 @@ package vazkii.quark.world.feature;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.monster.EntityBlaze;
 import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import vazkii.quark.base.module.Feature;
 
 public class NaturalBlazesInNether extends Feature {
 
 	int weight, min, max;
+	boolean restrictToNetherrack;
 
 	@Override
 	public void setupConfig() {
-		weight = loadPropInt("Spawn Weight", "", 20);
+		weight = loadPropInt("Spawn Weight", "", 10);
 		min = loadPropInt("Smallest spawn group", "", 1);
 		max = loadPropInt("Largest spawn group", "", 3);
+		restrictToNetherrack = loadPropBool("Block restrictions", "Make naturally spawned blazes only spawn in netherrack", true);
 	}
 
 	@Override
@@ -33,9 +41,27 @@ public class NaturalBlazesInNether extends Feature {
 		Biomes.HELL.getSpawnableList(EnumCreatureType.MONSTER).add(new SpawnListEntry(EntityBlaze.class, weight, min, max));
 	}
 	
+	@SubscribeEvent
+	public void onSpawn(LivingSpawnEvent.CheckSpawn event) {
+		if(restrictToNetherrack && event.getEntityLiving() instanceof EntityBlaze && event.getResult() != Result.DENY && event.getEntityLiving().world instanceof WorldServer) {
+			EntityBlaze blaze = (EntityBlaze) event.getEntityLiving();
+			WorldServer world = (WorldServer) blaze.world;
+			BlockPos pos = blaze.getPosition();
+			boolean netherrack = world.getBlockState(pos.down()).getBlock() == Blocks.NETHERRACK;
+			boolean fortress = world.getChunkProvider().isInsideStructure(world, "Fortress", pos);
+			if(!fortress && !netherrack)
+				event.setResult(Result.DENY);
+		}
+	}
+
+	@Override
+	public boolean hasSubscriptions() {
+		return true;
+	}
+	
 	@Override
 	public boolean requiresMinecraftRestartToEnable() {
 		return true;
 	}
-
+	
 }
