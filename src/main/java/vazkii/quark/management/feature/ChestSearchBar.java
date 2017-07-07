df0aadeedbf508477e87191315a13a06c36594ae
Vasco Lavos <vazkii@hotmail.com>
2017-07-07 01:00:11 +0100

    Added an API hook for the search bar

--- src/main/java/vazkii/quark/management/feature/ChestSearchBar.java
+++ src/main/java/vazkii/quark/management/feature/ChestSearchBar.java
@@ -18,6 +18,7 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import vazkii.quark.api.IItemSearchBar;
 import vazkii.quark.base.handler.GuiFactory;
 import vazkii.quark.base.module.Feature;
 import vazkii.quark.management.client.gui.GuiButtonChest;
@@ -31,13 +32,17 @@ public class ChestSearchBar extends Feature {
 	@SubscribeEvent
 	public void initGui(GuiScreenEvent.InitGuiEvent.Post event) {
 		GuiScreen gui = event.getGui();
-		if(gui instanceof GuiChest || gui instanceof GuiShulkerBox) {
+		boolean callback = gui instanceof IItemSearchBar;
+		if(callback || gui instanceof GuiChest || gui instanceof GuiShulkerBox) {
 			GuiContainer chest = (GuiContainer) gui;
 			searchBar = new GuiTextField(12831, gui.mc.fontRenderer, chest.getGuiLeft() + 80, chest.getGuiTop() + 5, 88, 10);
 			searchBar.setText(text);
 			searchBar.setFocused(false);
 			searchBar.setMaxStringLength(32);
 			searchBar.setEnableBackgroundDrawing(false);
+			
+			if(callback)
+				((IItemSearchBar) gui).onSearchBarAdded(searchBar);
 		} else searchBar = null;
 	}
 	
@@ -83,8 +88,8 @@ public class ChestSearchBar extends Feature {
 		}
 	}
 	
-	private void renderElements(GuiScreen gui ) {
-		drawBackground(searchBar.x - 1, searchBar.y - 1);
+	private void renderElements(GuiScreen gui) {
+		drawBackground(gui, searchBar.x - 1, searchBar.y - 1);
 		searchBar.drawTextBox();
 		
 		if(!text.isEmpty()) {
@@ -109,11 +114,13 @@ public class ChestSearchBar extends Feature {
 		}
 	}
 	
-	private void drawBackground(int x, int y) {
-		Minecraft mc = Minecraft.getMinecraft();
+	private void drawBackground(GuiScreen gui, int x, int y) {
+		if(gui instanceof IItemSearchBar && ((IItemSearchBar) gui).renderBackground(x, y))
+			return;
+		
 		GlStateManager.color(1F, 1F, 1F, 1F);
 		GlStateManager.disableLighting();
-		mc.getTextureManager().bindTexture(GuiButtonChest.GENERAL_ICONS_RESOURCE);
+		gui.mc.getTextureManager().bindTexture(GuiButtonChest.GENERAL_ICONS_RESOURCE);
 		Gui.drawModalRectWithCustomSizedTexture(x, y, 0, 244, 90, 12, 256, 256);
 	}
 	
