70435e7494fda4ee104d273d3983f2a272e0826d
Vasco Lavos <vazkii@hotmail.com>
2017-07-22 19:10:33 +0100

    WIP trash can

---
+++ src/main/java/vazkii/quark/management/client/gui/GuiButtonTrash.java
@@ -0,0 +1,78 @@
+package vazkii.quark.management.client.gui;
+
+import java.util.List;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSoundRecord;
+import net.minecraft.client.audio.SoundHandler;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.translation.I18n;
+import scala.actors.threadpool.Arrays;
+import vazkii.arl.util.RenderHelper;
+import vazkii.quark.base.client.IParentedGui;
+import vazkii.quark.management.feature.DeleteItems;
+
+public class GuiButtonTrash extends GuiButton implements IParentedGui {
+
+	public final GuiScreen parent;
+	public boolean ready;
+
+	public GuiButtonTrash(GuiScreen parent, int id, int par2, int par3) {
+		super(id, par2, par3, 16, 16, "");
+		this.parent = parent;
+	}
+	
+	@Override
+	public void drawButton(Minecraft par1Minecraft, int par2, int par3, float pticks) {
+		hovered = par2 >= x && par3 >= y && par2 < x + width && par3 < y + height;
+		int k = getHoverState(hovered);
+
+		int u = 0;
+		int v = 192;
+		
+		if(parent instanceof GuiContainer) {
+			EntityPlayer player = par1Minecraft.player;
+			ItemStack hovered = player.inventory.getItemStack();
+			if(DeleteItems.canItemBeDeleted(hovered))
+				u += 16;
+		}
+		
+		par1Minecraft.renderEngine.bindTexture(GuiButtonChest.GENERAL_ICONS_RESOURCE);
+		GlStateManager.color(1F, 1F, 1F, 1F);
+		drawIcon(u, v);
+		
+		ready = (k == 2 && u != 0);
+		if(ready) {
+			GlStateManager.pushMatrix();
+			String tooltip = I18n.translateToLocal("quarkmisc.trashButtonOpen"); 
+			int len = Minecraft.getMinecraft().fontRenderer.getStringWidth(tooltip);
+			int tooltipShift = 2;
+			List<String> tooltipList = Arrays.asList(new String[]{ tooltip });
+			
+			RenderHelper.renderTooltip(par2 + tooltipShift, par3 + 8, tooltipList);
+			GlStateManager.popMatrix();
+		}
+	}
+
+	protected void drawIcon(int u, int v) {
+		drawTexturedModalRect(x, y, u, v, 16, 16);
+	}
+	
+	@Override
+    public void playPressSound(SoundHandler soundHandlerIn) {
+        // NO-OP
+    }
+
+	
+	@Override
+	public GuiScreen getParent() {
+		return parent;
+	}
+	
+}
--- src/main/java/vazkii/quark/management/feature/DeleteItems.java
+++ src/main/java/vazkii/quark/management/feature/DeleteItems.java
@@ -1,30 +1,90 @@
 package vazkii.quark.management.feature;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import vazkii.arl.network.NetworkHandler;
 import vazkii.quark.base.module.Feature;
 import vazkii.quark.base.module.ModuleLoader;
 import vazkii.quark.base.network.message.MessageDeleteItem;
+import vazkii.quark.management.client.gui.GuiButtonTrash;
 
 public class DeleteItems extends Feature {
 
-	boolean down;
+	boolean keyboardDown = false;
+	boolean mouseDown = false;
+	GuiButtonTrash trash;
+	
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void initGui(GuiScreenEvent.InitGuiEvent.Post event) {
+		trash = null;
+		if(event.getGui() instanceof GuiContainer) {
+			GuiContainer guiInv = (GuiContainer) event.getGui();
+			Container container = guiInv.inventorySlots;
+			EntityPlayer player = Minecraft.getMinecraft().player;
+			
+			boolean accept = guiInv instanceof GuiContainer;
+
+			if(!accept)
+				return;
+
+			int guiLeft = guiInv.getGuiLeft();
+			int guiTop = guiInv.getGuiTop();
+			int guiWidth = guiInv.getXSize();
+			int guiHeight = guiInv.getYSize();
+
+			for(Slot s : container.inventorySlots)
+				if(s.inventory == player.inventory && s.getSlotIndex() == 9) {
+					trash = new GuiButtonTrash(guiInv, 82424, guiLeft + guiWidth + 2, guiTop + guiHeight - 25);
+					event.getButtonList().add(trash);
+					break;
+				}
+		}
+	}
+	
+	@SubscribeEvent
+	public void mouseEvent(GuiScreenEvent.MouseInputEvent.Pre event) {
+		handleMouseclick(event);
+	}
+	
+	@SubscribeEvent
+	public void mouseEvent(MouseEvent event) {
+		handleMouseclick(event);
+	}
+	
+	private void handleMouseclick(Event event) {
+		mouseDown = Mouse.isButtonDown(0);
+		boolean click = mouseDown;
+
+		GuiScreen current = Minecraft.getMinecraft().currentScreen;
+		if(click && current instanceof GuiContainer) {
+			GuiContainer gui = (GuiContainer) current;
+			if(trash != null && trash.ready) {
+				event.setCanceled(true);
+			}
+		}
+	}
 	
 	@SubscribeEvent
 	public void keyboardEvent(GuiScreenEvent.KeyboardInputEvent.Post event) {
 		boolean down = Keyboard.isCreated() && Keyboard.isKeyDown(Keyboard.KEY_DELETE);
-		if(GuiScreen.isCtrlKeyDown() && down && !this.down && event.getGui() instanceof GuiContainer) {
+		if(GuiScreen.isCtrlKeyDown() && down && !this.keyboardDown && event.getGui() instanceof GuiContainer) {
 			GuiContainer gui = (GuiContainer) event.getGui();
 			Slot slot = gui.getSlotUnderMouse();
 			if(slot != null) {
@@ -40,17 +100,21 @@ public class DeleteItems extends Feature {
 				}
 			}
 		}
-		this.down = down;
+		this.keyboardDown = down;
 	}
 	
 	public static void deleteItem(EntityPlayer player, int slot) {
-		if(!ModuleLoader.isFeatureEnabled(DeleteItems.class) || slot >= player.inventory.mainInventory.size())
+		if(slot > player.inventory.mainInventory.size() || !canItemBeDeleted(player.inventory.getStackInSlot(slot)))
 			return;
 		
-		if(player.getName().equalsIgnoreCase("mcjty"))
-			slot = player.getEntityWorld().rand.nextInt(player.inventory.mainInventory.size());
+		if(slot == player.inventory.mainInventory.size())
+			player.inventory.setItemStack(ItemStack.EMPTY);
 
 		player.inventory.setInventorySlotContents(slot, ItemStack.EMPTY);
+	}
+	
+	public static boolean canItemBeDeleted(ItemStack stack) {
+		return ModuleLoader.isFeatureEnabled(DeleteItems.class) && !stack.isEmpty() && !FavoriteItems.isItemFavorited(stack);
 	}
 	
 	@Override
