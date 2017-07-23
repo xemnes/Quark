6cfdc7871f896d0d418527dbe2c421cd1b7ab61c
Vasco Lavos <vazkii@hotmail.com>
2017-07-23 21:39:14 +0100

    Added config settings to blacklist recipes from being unlocked and to force limited crafting

--- src/main/java/vazkii/quark/tweaks/feature/AutomaticRecipeUnlock.java
+++ src/main/java/vazkii/quark/tweaks/feature/AutomaticRecipeUnlock.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
@@ -17,10 +18,15 @@ public class AutomaticRecipeUnlock extends Feature {
 
 	List<String> ignored;
 	
+	boolean forceLimitedCrafting;
+	
 	@Override
 	public void setupConfig() {
 		String[] ignoredArr = loadPropStringList("Ignored Recipes", "A list of recipe names that should NOT be added in by default", new String[0]);
 		ignored = Arrays.asList(ignoredArr);
+		
+		forceLimitedCrafting = loadPropBool("Force Limited Crafting", "Set to true to force the doLimitedCrafting gamerule to true.\n"
+				+ "Combine this with the Ignored Recipes list to create a system where only a few selected recipes are locked.", false);
 	}
 	
 	@SubscribeEvent 
@@ -29,9 +35,12 @@ public class AutomaticRecipeUnlock extends Feature {
 			ArrayList<IRecipe> recipes = Lists.newArrayList(CraftingManager.REGISTRY);
 			recipes.removeIf((recipe) -> ignored.contains(recipe.getRegistryName().toString()));
 			((EntityPlayerMP) event.player).unlockRecipes(recipes);
+			
+			if(forceLimitedCrafting)
+				event.player.world.getGameRules().setOrCreateGameRule("doLimitedCrafting", "true");
 		}
 	}
-
+	
 	@Override
 	public boolean hasSubscriptions() {
 		return true;
