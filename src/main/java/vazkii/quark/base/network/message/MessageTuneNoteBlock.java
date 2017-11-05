d1269ff9f64c86d2ba1cbba849b50ade9a51b252
Vasco Lavos <vazkii@hotmail.com>
2017-11-05 20:54:25 +0000

    Fixed the note block GUI not altering vanilla note block play and tune stats. Closes #665

--- src/main/java/vazkii/quark/base/network/message/MessageTuneNoteBlock.java
+++ src/main/java/vazkii/quark/base/network/message/MessageTuneNoteBlock.java
@@ -1,5 +1,7 @@
 package vazkii.quark.base.network.message;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.stats.StatList;
 import net.minecraft.tileentity.TileEntityNote;
 import vazkii.arl.network.TileEntityMessage;
 import vazkii.quark.base.module.ModuleLoader;
@@ -24,16 +26,24 @@ public class MessageTuneNoteBlock extends TileEntityMessage<TileEntityNote> {
 			if(!ModuleLoader.isFeatureEnabled(NoteBlockInterface.class))
 				return;
 			
+			EntityPlayer player = context.getServerHandler().player;
 			byte old = tile.note;
+			boolean tuned = false;
 			
 			if(next)
 				tile.changePitch();
 			else {
 				tile.note = target;
-				if(net.minecraftforge.common.ForgeHooks.onNoteChange(tile, old))
+				if(net.minecraftforge.common.ForgeHooks.onNoteChange(tile, old)) {
 					tile.markDirty();
+					player.addStat(StatList.NOTEBLOCK_TUNED);
+					tuned = true;
+				}
 			}
 
+			if(!tuned)
+				player.addStat(StatList.NOTEBLOCK_PLAYED);
+			
 			tile.triggerNote(tile.getWorld(), pos);
 		};
 	}
