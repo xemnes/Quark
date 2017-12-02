b15bfae1f58f267e06d12c4a33161edfe5d18d96
Vasco Lavos <vazkii@hotmail.com>
2017-12-02 00:04:39 +0000

    Use the JEI wrench

--- src/main/java/vazkii/quark/base/client/gui/GuiButtonFeatureSettings.java
+++ src/main/java/vazkii/quark/base/client/gui/GuiButtonFeatureSettings.java
@@ -1,6 +1,9 @@
 package vazkii.quark.base.client.gui;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
+import vazkii.quark.base.lib.LibMisc;
 
 public class GuiButtonFeatureSettings extends GuiButton {
 
@@ -9,6 +12,13 @@ public class GuiButtonFeatureSettings extends GuiButton {
 	public GuiButtonFeatureSettings(int x, int y, String category) {
 		super(0, x, y, 20, 20, "C");
 		this.category = category;
+	}
+	
+	@Override
+	public void drawCenteredString(FontRenderer fontRendererIn, String text, int x, int y, int color) {
+		Minecraft mc = Minecraft.getMinecraft();
+		mc.getTextureManager().bindTexture(LibMisc.GENERAL_ICONS_RESOURCE);
+		drawTexturedModalRect(this.x + 2, this.y + 2, 32, 228, 16, 16);
 	}
 
 }
