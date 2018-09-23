a88529117437c047c4bd8c3b6eff99d44d51233b
Vasco Lavos <vazkii@hotmail.com>
2018-09-23 15:55:39 +0100

    Mobs are no longer allowed to path through speleothems. Closes #1007

--- src/main/java/vazkii/quark/world/block/BlockSpeleothem.java
+++ src/main/java/vazkii/quark/world/block/BlockSpeleothem.java
@@ -72,6 +72,11 @@ public class BlockSpeleothem extends BlockMod implements IQuarkBlock {
 		return true;
 	}
 	
+	@Override
+    public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+        return false;
+    }
+	
 	private int getBearing(IBlockAccess world, BlockPos pos) {
 		return Math.max(getStrength(world, pos.down()), getStrength(world, pos.up()));
 	}
