6a6e9d843da9d0c40181192471537977e04958af
Vasco Lavos <vazkii@hotmail.com>
2018-11-27 05:13:17 +0000

    Massively simplyify this horrible logic

--- src/main/java/vazkii/quark/automation/feature/PistonsPushPullItems.java
+++ src/main/java/vazkii/quark/automation/feature/PistonsPushPullItems.java
@@ -2,14 +2,11 @@ package vazkii.quark.automation.feature;
 
 import java.util.List;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.BlockPistonExtension;
 import net.minecraft.block.BlockPistonExtension.EnumPistonType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityPiston;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
@@ -32,50 +29,34 @@ public class PistonsPushPullItems extends Feature {
 	public static void onPistonUpdate(TileEntityPiston piston) {
 		if(!ModuleLoader.isFeatureEnabled(PistonsPushPullItems.class) || piston.getWorld().isRemote)
 			return;
-		
-		boolean sticky = piston.getPistonState().getBlock() == Blocks.STICKY_PISTON; 
-		
+
+		IBlockState state = piston.getPistonState();
+		boolean pulling = state.getBlock() != Blocks.PISTON_HEAD;
+		boolean sticky = state.getBlock() == Blocks.STICKY_PISTON || (state.getPropertyKeys().contains(BlockPistonExtension.TYPE) && state.getValue(BlockPistonExtension.TYPE) == EnumPistonType.STICKY); 
+
 		EnumFacing face = piston.getFacing();
-		AxisAlignedBB aabb = new AxisAlignedBB(piston.getPos().offset(face, sticky ? 2 : 1));
+		AxisAlignedBB aabb = new AxisAlignedBB(piston.getPos().offset(face, pulling ? 2 : 1));
 		List<EntityItem> items = piston.getWorld().getEntitiesWithinAABB(EntityItem.class, aabb);
-		face = face.getOpposite();
-		
+
 		for(EntityItem entity : items)
-			onEntityHandled(entity, face);
+			onEntityHandled(entity, face, sticky, pulling);
 	}
 
-	private static void onEntityHandled(EntityItem entity, EnumFacing face) {
+	private static void onEntityHandled(EntityItem entity, EnumFacing face, boolean sticky, boolean pulling) {
+		if(sticky)
+			face = face.getOpposite();
+		
 		World world = entity.getEntityWorld();
 		BlockPos pos = entity.getPosition();
 		BlockPos offsetPos1 = pos.offset(face);
-		if(world.isBlockLoaded(offsetPos1)) {
-			IBlockState state = world.getBlockState(offsetPos1);
-			//check for moving blocks or normal piston heads pushing *in* to this item
-			if(state.getBlock() == Blocks.PISTON_EXTENSION && state.getValue(BlockDirectional.FACING) == face.getOpposite() && state.getValue(BlockPistonExtension.TYPE) == EnumPistonType.DEFAULT)
-				nudgeItem(world, entity, face.getOpposite(), true);
-		}
+		
+		if(!sticky && !pulling)
+			nudgeItem(world, entity, face, true);
 
-		boolean closeToEdge = new BlockPos(entity.posX + face.getFrontOffsetX() * .5, entity.posY + face.getFrontOffsetY() * .5, entity.posZ + face.getFrontOffsetZ() * .5).equals(offsetPos1);
-
-		if(closeToEdge) {
-			BlockPos offsetPos2 = pos.offset(face, 2);
-			if(world.isBlockLoaded(offsetPos2)) {
-				IBlockState state = world.getBlockState(offsetPos2);
-				if(state.getBlock() == Blocks.PISTON_EXTENSION) {
-					//check for adjacent moving sticky piston heads pulling *away* from this item
-					if(state.getValue(BlockDirectional.FACING) == face.getOpposite() && state.getValue(BlockPistonExtension.TYPE) == EnumPistonType.STICKY) {
-						//verify it's a moving head, not just any old block
-						TileEntity tile = world.getTileEntity(offsetPos2);
-						if(tile instanceof TileEntityPiston) {
-							TileEntityPiston movingBlockTile = (TileEntityPiston) tile;
-							IBlockState movingBlockState = movingBlockTile.getPistonState();
-							Block movingBlock = movingBlockState.getBlock();
-							if(movingBlock == Blocks.STICKY_PISTON)
-								nudgeItem(world, entity, face, false);
-						}
-					}
-				}
-			}
+		if(sticky && pulling) {
+			boolean closeToEdge = new BlockPos(entity.posX + face.getFrontOffsetX() * .5, entity.posY + face.getFrontOffsetY() * .5, entity.posZ + face.getFrontOffsetZ() * .5).equals(offsetPos1);
+			if(closeToEdge)
+				nudgeItem(world, entity, face, false);
 		}
 	}
 
