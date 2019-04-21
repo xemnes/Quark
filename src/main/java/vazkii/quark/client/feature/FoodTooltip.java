7ea530cd5d2bc31a5ebb6861723e52f4e20b0f04
yrsegal@gmail.com <yrsegal@gmail.com>
2019-04-21 10:10:11 -0400

    Fix BWM api being bundled with the quark jar
    Closes #1198

--- src/main/java/vazkii/quark/client/feature/FoodTooltip.java
+++ src/main/java/vazkii/quark/client/feature/FoodTooltip.java
@@ -12,6 +12,7 @@ import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import vazkii.quark.base.lib.LibObfuscation;
 import vazkii.quark.base.module.Feature;
@@ -23,6 +24,7 @@ public class FoodTooltip extends Feature {
 	int divisor = 2;
 	
 	@SubscribeEvent
+	@Optional.Method(modid = "betterwithmods")
 	public void bwmFeatureEnabled(FeatureEnabledEvent event) {
 		if(event.getFeature().equals("hchunger") && event.isEnabled())
 			divisor = 12;
@@ -34,20 +36,20 @@ public class FoodTooltip extends Feature {
 			int pips = ((ItemFood) event.getItemStack().getItem()).getHealAmount(event.getItemStack());
 			int len = (int) Math.ceil((double) pips / divisor);
 			
-			String s = " ";
+			StringBuilder s = new StringBuilder(" ");
 			for(int i = 0; i < len; i++)
-				s += "  ";
+				s.append("  ");
 			
 			List<String> tooltip = event.getToolTip();
 			if(tooltip.isEmpty())
-				tooltip.add(s);
-			else tooltip.add(1, s);
+				tooltip.add(s.toString());
+			else tooltip.add(1, s.toString());
 		}
 	}
 
 	@SubscribeEvent
 	public void renderTooltip(RenderTooltipEvent.PostText event) {
-		if(event.getStack() != null && event.getStack().getItem() instanceof ItemFood) {
+		if(!event.getStack().isEmpty() && event.getStack().getItem() instanceof ItemFood) {
 			GlStateManager.pushMatrix();
 			GlStateManager.color(1F, 1F, 1F);
 			Minecraft mc = Minecraft.getMinecraft();
@@ -56,7 +58,7 @@ public class FoodTooltip extends Feature {
 			int pips = food.getHealAmount(event.getStack());
 			
 			PotionEffect eff = ObfuscationReflectionHelper.getPrivateValue(ItemFood.class, food, LibObfuscation.POTION_ID);
-			boolean poison = eff != null && eff.getPotion() != null && eff.getPotion().isBadEffect();
+			boolean poison = eff != null && eff.getPotion().isBadEffect();
 
 			for(int i = 0; i < Math.ceil((double) pips / divisor); i++) {
 				int x = event.getX() + i * 9 - 2;
