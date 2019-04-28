f54dd3a6212df3ca66a0202cc8210559c0b0eff1
Vasco Lavos <vazkii@hotmail.com>
2019-04-28 19:42:21 +0100

    aaaaaaaaaaaaaaaaaaa

--- src/main/java/vazkii/quark/world/world/ArcheologistHouseGenerator.java
+++ src/main/java/vazkii/quark/world/world/ArcheologistHouseGenerator.java
@@ -1,10 +1,13 @@
 package vazkii.quark.world.world;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
@@ -13,12 +16,9 @@ import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.structure.template.PlacementSettings;
 import net.minecraft.world.gen.structure.template.Template;
 import net.minecraftforge.fml.common.IWorldGenerator;
+import vazkii.arl.util.RotationHandler;
 import vazkii.quark.world.entity.EntityArcheologist;
 import vazkii.quark.world.feature.Archeologist;
-
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Random;
 
 public class ArcheologistHouseGenerator implements IWorldGenerator {
 
@@ -63,7 +63,7 @@ public class ArcheologistHouseGenerator implements IWorldGenerator {
 		MinecraftServer server = world.getMinecraftServer();
 		Template template = world.getStructureTemplateManager().getTemplate(server, Archeologist.HOUSE_STRUCTURE);
 		PlacementSettings settings = new PlacementSettings();
-		settings.setRotation(Rotation.values()[face.getHorizontalIndex()]);
+		settings.setRotation(RotationHandler.getRotationFromFacing(face));
 		
 		BlockPos placePos = pos.offset(face, 7);
 		template.addBlocksToWorld(world, placePos, settings);
