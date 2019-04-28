4f6414538181c2cda5614182b5084a58143f07b6
yrsegal@gmail.com <yrsegal@gmail.com>
2019-04-28 15:40:01 -0400

    Fix #1186
    Walls are intended to be in Misc, as that's how vanilla does it.

--- src/main/java/vazkii/quark/building/block/slab/BlockSoulSandstoneSlab.java
+++ src/main/java/vazkii/quark/building/block/slab/BlockSoulSandstoneSlab.java
@@ -2,6 +2,7 @@ package vazkii.quark.building.block.slab;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import vazkii.quark.base.block.BlockQuarkSlab;
 
 public class BlockSoulSandstoneSlab extends BlockQuarkSlab {
@@ -9,7 +10,8 @@ public class BlockSoulSandstoneSlab extends BlockQuarkSlab {
 	public BlockSoulSandstoneSlab(boolean doubleSlab) {
 		super("soul_sandstone_slab", Material.ROCK, doubleSlab);
 		setHardness(1F);
-		setSoundType(SoundType.STONE);	
+		setSoundType(SoundType.STONE);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
 	}
 
 }
