0c00143103cbd2d28322cdbef5652a3125d1992c
yrsegal@gmail.com <yrsegal@gmail.com>
2019-05-10 13:50:20 -0400

    add a version dependency string to autoreglib

--- src/main/java/vazkii/quark/base/lib/LibMisc.java
+++ src/main/java/vazkii/quark/base/lib/LibMisc.java
@@ -22,7 +22,7 @@ public final class LibMisc {
 	public static final String MOD_NAME = MOD_ID;
 	public static final String BUILD = "GRADLE:BUILD";
 	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
-	public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2779,);required-before:autoreglib;after:jei@[4.6.0,);";
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2779,);required-before:autoreglib@[1.3-28,);after:jei@[4.6.0,);";
 	public static final String PREFIX_MOD = MOD_ID + ":";
 
 	// Proxy Constants
