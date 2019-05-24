0e832f40366f8dfacaa0343bb8c6cf571f6758c9
yrsegal@gmail.com <yrsegal@gmail.com>
2019-05-24 10:21:47 -0400

    The issue which this solved was fixed upstream

--- src/main/java/vazkii/quark/base/asm/ClassTransformer.java
+++ src/main/java/vazkii/quark/base/asm/ClassTransformer.java
@@ -580,7 +580,7 @@ public class ClassTransformer implements IClassTransformer, Opcodes {
 		}
 
 		if (didAnything) {
-			ClassWriter writer = new SafeClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
 			node.accept(writer);
 			return writer.toByteArray();
 		}
@@ -684,45 +684,6 @@ public class ClassTransformer implements IClassTransformer, Opcodes {
 			return FMLDeobfuscatingRemapper.INSTANCE.mapMethodName(owner, srgName, funcDesc);
 		}
 
-	}
-
-	/**
-	 * Safe class writer.
-	 * The way COMPUTE_FRAMES works may require loading additional classes. This can cause ClassCircularityErrors.
-	 * The override for getCommonSuperClass will ensure that COMPUTE_FRAMES works properly by using the right ClassLoader.
-	 * <p>
-	 * Code from: https://github.com/JamiesWhiteShirt/clothesline/blob/master/src/core/java/com/jamieswhiteshirt/clothesline/core/SafeClassWriter.java
-	 */
-	public static class SafeClassWriter extends ClassWriter {
-		public SafeClassWriter(int flags) {
-			super(flags);
-		}
-
-		@Override
-		protected String getCommonSuperClass(String type1, String type2) {
-			Class<?> c, d;
-			ClassLoader classLoader = Launch.classLoader;
-			try {
-				c = Class.forName(type1.replace('/', '.'), false, classLoader);
-				d = Class.forName(type2.replace('/', '.'), false, classLoader);
-			} catch (Exception e) {
-				throw new RuntimeException(e.toString());
-			}
-			if (c.isAssignableFrom(d)) {
-				return type1;
-			}
-			if (d.isAssignableFrom(c)) {
-				return type2;
-			}
-			if (c.isInterface() || d.isInterface()) {
-				return "java/lang/Object";
-			} else {
-				do {
-					c = c.getSuperclass();
-				} while (!c.isAssignableFrom(d));
-				return c.getName().replace('.', '/');
-			}
-		}
 	}
 
 	// Basic interface aliases to not have to clutter up the code with generics over and over again
