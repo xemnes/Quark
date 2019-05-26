b7755e6875cd23d296788cd637f897a8f1688c5b
yrsegal@gmail.com <yrsegal@gmail.com>
2019-05-26 07:59:39 -0400

    Fix #1281

--- src/main/java/vazkii/quark/client/render/RenderItemFlashing.java
+++ src/main/java/vazkii/quark/client/render/RenderItemFlashing.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderEntityItem;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import vazkii.quark.client.feature.ItemsFlashBeforeExpiring;
 
@@ -15,7 +16,11 @@ public class RenderItemFlashing extends RenderEntityItem {
 	
 	@Override
 	public void doRender(EntityItem entity, double x, double y, double z, float entityYaw, float partialTicks) {
-		int timeLeft = entity.lifespan - entity.getAge();
+		ItemStack item = entity.getItem();
+		int lifespan = entity.lifespan;
+		if (lifespan == 6000 && !item.isEmpty())
+			lifespan = item.getItem().getEntityLifespan(item, entity.world);
+		int timeLeft = lifespan - entity.getAge();
 		if(timeLeft < ItemsFlashBeforeExpiring.minTime && timeLeft % 20 < 8 && entity.ticksExisted > 1)
 			return;
 		
