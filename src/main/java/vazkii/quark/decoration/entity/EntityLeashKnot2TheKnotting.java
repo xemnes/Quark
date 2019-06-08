4bf5eb2ec8e2dd8270b58cb1791c2ee6150cc26d
yrsegal@gmail.com <yrsegal@gmail.com>
2019-06-08 19:46:56 -0400

    Close #1296

--- src/main/java/vazkii/quark/decoration/entity/EntityLeashKnot2TheKnotting.java
+++ src/main/java/vazkii/quark/decoration/entity/EntityLeashKnot2TheKnotting.java
@@ -8,10 +8,13 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
 
 public class EntityLeashKnot2TheKnotting extends EntityLiving {
 
@@ -23,16 +26,22 @@ public class EntityLeashKnot2TheKnotting extends EntityLiving {
 	}
 
 	@Override
+	public boolean attackEntityFrom(@Nonnull DamageSource source, float amount) {
+		dismantle(!source.isCreativePlayer());
+		return true;
+	}
+
+	@Override
 	public void onUpdate() {
 		super.onUpdate();
 		
 		IBlockState state = world.getBlockState(new BlockPos(posX, posY, posZ));
 		if(!(state.getBlock() instanceof BlockFence)) {
-			dismantle();
+			dismantle(true);
 		} else {
 			Entity holder = getLeashHolder();
-			if(holder.isDead)
-				dismantle();
+			if(holder == null || holder.isDead)
+				dismantle(true);
 			else if(holder.posY < posY && holder instanceof EntityLeashKnot) {
 				double targetX = holder.posX;
 				double targetY = holder.posY;
@@ -52,17 +61,17 @@ public class EntityLeashKnot2TheKnotting extends EntityLiving {
 		if(!world.isRemote) {
 			Entity holder = getLeashHolder();
 			holder.setDead();
-			dismantle();
-			if(!player.isCreative())
-				dropItem(Items.LEAD, 1);
+			dismantle(!player.isCreative());
 		}
 		
 		return true;
 	}
 	
-	private void dismantle() {
+	private void dismantle(boolean drop) {
 		setDead();
 		world.playSound(null, getPosition(), SoundEvents.ENTITY_LEASHKNOT_BREAK, SoundCategory.BLOCKS, 1F, 1F);
+		if(drop)
+			dropItem(Items.LEAD, 1);
 	}
 
 }
