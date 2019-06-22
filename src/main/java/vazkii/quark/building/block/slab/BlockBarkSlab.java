ca45e4e54d67552af127fe62a045543fe3ee10cf
yrsegal@gmail.com <yrsegal@gmail.com>
2019-06-23 01:08:29 +0300

    Close #1328, #1329

--- src/main/java/vazkii/quark/building/block/slab/BlockBarkSlab.java
+++ src/main/java/vazkii/quark/building/block/slab/BlockBarkSlab.java
@@ -3,6 +3,9 @@ package vazkii.quark.building.block.slab;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import vazkii.arl.interf.IRecipeGrouped;
 import vazkii.quark.base.block.BlockQuarkSlab;
 import vazkii.quark.building.block.BlockBark;
@@ -20,5 +23,4 @@ public class BlockBarkSlab extends BlockQuarkSlab implements IRecipeGrouped {
 	public String getRecipeGroup() {
 		return "bark_slab";
 	}
-	
 }
