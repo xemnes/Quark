144ef9d62fe5618f8a61511ee28002134a764581
yrsegal@gmail.com <yrsegal@gmail.com>
2019-06-24 23:06:29 +0300

    Flowers are now edible >:(

--- src/main/java/vazkii/quark/base/item/ItemQuarkFood.java
+++ src/main/java/vazkii/quark/base/item/ItemQuarkFood.java
@@ -1,7 +1,5 @@
 package vazkii.quark.base.item;
 
-import javax.annotation.Nonnull;
-
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
@@ -10,19 +8,27 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import vazkii.arl.util.ProxyRegistry;
 
+import javax.annotation.Nonnull;
+
 public class ItemQuarkFood extends ItemFood implements IQuarkItem {
 
 	private final String bareName;
+	private final String[] variants;
 
-	public ItemQuarkFood(String name, int amount, float saturation, boolean isWolfFood) {
+	public ItemQuarkFood(String name, int amount, float saturation, boolean isWolfFood, String... variants) {
 		super(amount, saturation, isWolfFood);
 		setTranslationKey(name);
 		bareName = name;
+		if (variants == null || variants.length == 0)
+			variants = new String[] { bareName };
+		if (variants.length > 1)
+			setHasSubtypes(true);
+		this.variants = variants;
 		setCreativeTab(CreativeTabs.FOOD);
 	}
 	
-	public ItemQuarkFood(String name, int amount, float saturation) {
-		this(name, amount, saturation, false);
+	public ItemQuarkFood(String name, int amount, float saturation, String... variants) {
+		this(name, amount, saturation, false, variants);
 	}
 	
 	@Nonnull
@@ -38,18 +44,27 @@ public class ItemQuarkFood extends ItemFood implements IQuarkItem {
 	@Nonnull
 	@Override
 	public String getTranslationKey(ItemStack par1ItemStack) {
-		return "item." + getPrefix() + bareName;
+		int dmg = par1ItemStack.getItemDamage();
+		String[] variants = getVariants();
+
+		String name;
+		if(dmg >= variants.length)
+			name = bareName;
+		else name = variants[dmg];
+
+		return "item." + getPrefix() + name;
 	}
 
 	@Override
 	public void getSubItems(@Nonnull CreativeTabs tab, @Nonnull NonNullList<ItemStack> subItems) {
 		if(isInCreativeTab(tab))
-			subItems.add(new ItemStack(this, 1, 0));
+			for(int i = 0; i < getVariants().length; i++)
+				subItems.add(new ItemStack(this, 1, i));
 	}
 
 	@Override
 	public String[] getVariants() {
-		return new String[] { bareName };
+		return variants;
 	}
 
 }
--- src/main/java/vazkii/quark/world/item/ItemRootFlower.java
+++ src/main/java/vazkii/quark/world/item/ItemRootFlower.java
@@ -1,10 +1,10 @@
 package vazkii.quark.world.item;
 
 import net.minecraft.creativetab.CreativeTabs;
-import vazkii.arl.item.ItemMod;
 import vazkii.quark.base.item.IQuarkItem;
+import vazkii.quark.base.item.ItemQuarkFood;
 
-public class ItemRootFlower extends ItemMod implements IQuarkItem {
+public class ItemRootFlower extends ItemQuarkFood implements IQuarkItem {
 
 	private static final String[] VARIANTS = {
 			"root_blue_flower",
@@ -13,7 +13,7 @@ public class ItemRootFlower extends ItemMod implements IQuarkItem {
 	};
 	
 	public ItemRootFlower() {
-		super("root_flower", VARIANTS);
+		super("root_flower", 3, 0.4F, VARIANTS);
 		setCreativeTab(CreativeTabs.MATERIALS);
 	}
 
