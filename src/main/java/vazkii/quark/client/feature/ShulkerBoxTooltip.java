213eae16ed6bd9815de44cbab3e34f33052c06ff
yrsegal@gmail.com <yrsegal@gmail.com>
2019-06-24 10:23:43 +0300

    Close #1334

--- src/main/java/vazkii/quark/client/feature/ShulkerBoxTooltip.java
+++ src/main/java/vazkii/quark/client/feature/ShulkerBoxTooltip.java
@@ -9,21 +9,29 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.inventory.ItemStackHelper;
-import net.minecraft.item.*;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemDye;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.NonNullList;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 import vazkii.arl.util.ItemNBTHelper;
 import vazkii.quark.base.module.Feature;
+import vazkii.quark.management.feature.RightClickAddToShulkerBox;
 
-import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShulkerBoxTooltip extends Feature {
 
@@ -31,15 +39,27 @@ public class ShulkerBoxTooltip extends Feature {
 
 	public static boolean useColors, requireShift;
 
+	public static List<ResourceLocation> shulkerBoxes;
+	public static boolean dropoffAnyShulkerBox;
+
 	@Override
 	public void setupConfig() {
+		String[] shulkerArr = loadPropStringList("Shulker Boxes",
+				"Blocks which should be interpreted as Shulker Boxes.",
+				RightClickAddToShulkerBox.getBasicShulkerBoxes());
+		shulkerBoxes = Arrays.stream(shulkerArr).map(ResourceLocation::new).collect(Collectors.toList());
+
+		dropoffAnyShulkerBox = loadPropBool("Dropoff to Any Shulker Box",
+				"Allow anything with 'shulker_box' in its item identifier to be treated as a shulker box?", true);
+
 		useColors = loadPropBool("Use Colors", "", true);
 		requireShift = loadPropBool("Needs Shift to be visible", "", false);
 	}
 
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void makeTooltip(ItemTooltipEvent event) {
-		if(!event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof ItemShulkerBox && event.getItemStack().hasTagCompound()) {
+		if(RightClickAddToShulkerBox.isShulkerBox(event.getItemStack(), shulkerBoxes, dropoffAnyShulkerBox) && event.getItemStack().hasTagCompound()) {
 			NBTTagCompound cmp = ItemNBTHelper.getCompound(event.getItemStack(), "BlockEntityTag", true);
 			if(cmp != null && cmp.hasKey("Items", 9)) {
 				List<String> tooltip = event.getToolTip();
@@ -57,77 +77,147 @@ public class ShulkerBoxTooltip extends Feature {
 		}
 	}
 
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public void renderTooltip(RenderTooltipEvent.PostText event) {
-		if(!event.getStack().isEmpty() && event.getStack().getItem() instanceof ItemShulkerBox && event.getStack().hasTagCompound() && (!requireShift || GuiScreen.isShiftKeyDown())) {
+		if(RightClickAddToShulkerBox.isShulkerBox(event.getStack(), shulkerBoxes, dropoffAnyShulkerBox) && event.getStack().hasTagCompound() && (!requireShift || GuiScreen.isShiftKeyDown())) {
+			Minecraft mc = Minecraft.getMinecraft();
+
 			NBTTagCompound cmp = ItemNBTHelper.getCompound(event.getStack(), "BlockEntityTag", true);
-			if(cmp != null && cmp.hasKey("Items", 9)) {
-				ItemStack currentBox = event.getStack();
-				int currentX = event.getX() - 5;
-				int currentY = event.getY() - 70;
-				
-				int texWidth = 172;
-				int texHeight = 64;
-				
-				if(currentY < 0)
-					currentY = event.getY() + event.getLines().size() * 10 + 5;
-				
-				ScaledResolution res = new ScaledResolution(Minecraft.getMinecraft());
-				int right = currentX + texWidth;
-				if(right  > res.getScaledWidth())
-					currentX -= (right - res.getScaledWidth());
-				
-				GlStateManager.pushMatrix();
-				RenderHelper.enableStandardItemLighting();
-				GlStateManager.enableRescaleNormal();
-				GlStateManager.color(1F, 1F, 1F);
-				GlStateManager.translate(0, 0, 700);
+			if (cmp != null) {
+				TileEntity te = TileEntity.create(mc.world, cmp);
+				if (te != null && te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)) {
+					ItemStack currentBox = event.getStack();
+					int currentX = event.getX() - 5;
+					int currentY = event.getY() - 70;
 
-				Minecraft mc = Minecraft.getMinecraft();
-				mc.getTextureManager().bindTexture(WIDGET_RESOURCE);
+					IItemHandler capability = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+					assert capability != null;
 
-				RenderHelper.disableStandardItemLighting();
-				
-				if(useColors) {
-					EnumDyeColor dye = ((BlockShulkerBox) ((ItemBlock) currentBox.getItem()).getBlock()).getColor();
-					int color = ItemDye.DYE_COLORS[dye.getDyeDamage()];
-					Color colorObj = new Color(color);
-					GlStateManager.color(colorObj.getRed() / 255F, colorObj.getGreen() / 255F, colorObj.getBlue() / 255F);
-				}
-				Gui.drawModalRectWithCustomSizedTexture(currentX, currentY, 0, 0, texWidth, texHeight, 256, 256);
-				
-				GlStateManager.color(1F, 1F, 1F);
-
-				NonNullList<ItemStack> itemList = NonNullList.withSize(27, ItemStack.EMPTY);
-				ItemStackHelper.loadAllItems(cmp, itemList);
-
-				RenderItem render = mc.getRenderItem();
-
-				RenderHelper.enableGUIStandardItemLighting();
-				GlStateManager.enableDepth();
-				int i = 0;
-				for(ItemStack itemstack : itemList) {
-					int xp = currentX + 6 + (i % 9) * 18;
-					int yp = currentY + 6 + (i / 9) * 18;
-					
-					if(!itemstack.isEmpty()) {
-						render.renderItemAndEffectIntoGUI(itemstack, xp, yp);
-						render.renderItemOverlays(mc.fontRenderer, itemstack, xp, yp);
+					int size = capability.getSlots();
+					int[] dims = { Math.min(size, 9), Math.max(size / 9, 1) };
+					for (int[] testAgainst : TARGET_RATIOS) {
+						if (testAgainst[0] * testAgainst[1] == size) {
+							dims = testAgainst;
+							break;
+						}
 					}
-					
-					if(!ChestSearchBar.namesMatch(itemstack, ChestSearchBar.text)) {
-						GlStateManager.disableDepth();
-						Gui.drawRect(xp, yp, xp + 16, yp + 16, 0xAA000000);
-					}
-					
-					i++;
-				}
 
-				GlStateManager.disableDepth();
-				GlStateManager.disableRescaleNormal();
-				GlStateManager.popMatrix();
+					int texWidth = CORNER * 2 + EDGE * dims[0];
+
+					if (currentY < 0)
+						currentY = event.getY() + event.getLines().size() * 10 + 5;
+
+					ScaledResolution res = new ScaledResolution(mc);
+					int right = currentX + texWidth;
+					if (right > res.getScaledWidth())
+						currentX -= (right - res.getScaledWidth());
+
+					GlStateManager.pushMatrix();
+					RenderHelper.enableStandardItemLighting();
+					GlStateManager.enableRescaleNormal();
+					GlStateManager.color(1F, 1F, 1F);
+					GlStateManager.translate(0, 0, 700);
+					mc.getTextureManager().bindTexture(WIDGET_RESOURCE);
+
+					RenderHelper.disableStandardItemLighting();
+
+					int color = -1;
+
+					if (useColors) {
+						EnumDyeColor dye = ((BlockShulkerBox) ((ItemBlock) currentBox.getItem()).getBlock()).getColor();
+						color = ItemDye.DYE_COLORS[dye.getDyeDamage()];
+					}
+
+					renderTooltipBackground(mc, currentX, currentY, dims[0], dims[1], color);
+
+					RenderItem render = mc.getRenderItem();
+
+					RenderHelper.enableGUIStandardItemLighting();
+					GlStateManager.enableDepth();
+					for (int i = 0; i < size; i++) {
+						ItemStack itemstack = capability.getStackInSlot(i);
+						int xp = currentX + 6 + (i % 9) * 18;
+						int yp = currentY + 6 + (i / 9) * 18;
+
+						if (!itemstack.isEmpty()) {
+							render.renderItemAndEffectIntoGUI(itemstack, xp, yp);
+							render.renderItemOverlays(mc.fontRenderer, itemstack, xp, yp);
+						}
+
+						if (!ChestSearchBar.namesMatch(itemstack, ChestSearchBar.text)) {
+							GlStateManager.disableDepth();
+							Gui.drawRect(xp, yp, xp + 16, yp + 16, 0xAA000000);
+						}
+					}
+
+					GlStateManager.disableDepth();
+					GlStateManager.disableRescaleNormal();
+					GlStateManager.popMatrix();
+				}
 			}
 		}
+	}
+
+	private static final int[][] TARGET_RATIOS = new int[][] {
+			{ 1, 1 },
+			{ 9, 3 },
+			{ 9, 5 },
+			{ 9, 6 },
+			{ 9, 8 },
+			{ 9, 9 },
+			{ 12, 9 }
+	};
+
+	private static final int CORNER = 5;
+	private static final int BUFFER = 1;
+	private static final int EDGE = 18;
+
+
+	public static void renderTooltipBackground(Minecraft mc, int x, int y, int width, int height, int color) {
+		mc.getTextureManager().bindTexture(WIDGET_RESOURCE);
+		GlStateManager.color(((color & 0xFF0000) >> 16) / 255f,
+				((color & 0x00FF00) >> 8) / 255f,
+				(color & 0x0000FF) / 255f);
+
+		RenderHelper.disableStandardItemLighting();
+
+		Gui.drawModalRectWithCustomSizedTexture(x, y,
+				0, 0,
+				CORNER, CORNER, 256, 256);
+		Gui.drawModalRectWithCustomSizedTexture(x + CORNER + EDGE * width, y + CORNER + EDGE * height,
+				CORNER + BUFFER + EDGE + BUFFER, CORNER + BUFFER + EDGE + BUFFER,
+				CORNER, CORNER, 256, 256);
+		Gui.drawModalRectWithCustomSizedTexture(x + CORNER + EDGE * width, y,
+				CORNER + BUFFER + EDGE + BUFFER, 0,
+				CORNER, CORNER, 256, 256);
+		Gui.drawModalRectWithCustomSizedTexture(x, y + CORNER + EDGE * height,
+				0, CORNER + BUFFER + EDGE + BUFFER,
+				CORNER, CORNER, 256, 256);
+		for (int row = 0; row < height; row++) {
+			Gui.drawModalRectWithCustomSizedTexture(x, y + CORNER + EDGE * row,
+					0, CORNER + BUFFER,
+					CORNER, EDGE, 256, 256);
+			Gui.drawModalRectWithCustomSizedTexture(x + CORNER + EDGE * width, y + CORNER + EDGE * row,
+					CORNER + BUFFER + EDGE + BUFFER, CORNER + BUFFER,
+					CORNER, EDGE, 256, 256);
+			for (int col = 0; col < width; col++) {
+				if (row == 0) {
+					Gui.drawModalRectWithCustomSizedTexture(x + CORNER + EDGE * col, y,
+							CORNER + BUFFER, 0,
+							EDGE, CORNER, 256, 256);
+					Gui.drawModalRectWithCustomSizedTexture(x + CORNER + EDGE * col, y + CORNER + EDGE * height,
+							CORNER + BUFFER, CORNER + BUFFER + EDGE + BUFFER,
+							EDGE, CORNER, 256, 256);
+				}
+
+				Gui.drawModalRectWithCustomSizedTexture(x + CORNER + EDGE * col, y + CORNER + EDGE * row,
+						CORNER + BUFFER, CORNER + BUFFER,
+						EDGE, EDGE, 256, 256);
+			}
+		}
+
+		GlStateManager.color(1F, 1F, 1F);
 	}
 
 	@Override
--- src/main/java/vazkii/quark/tweaks/feature/BlastproofShulkerBoxes.java
+++ src/main/java/vazkii/quark/tweaks/feature/BlastproofShulkerBoxes.java
@@ -1,20 +1,36 @@
 package vazkii.quark.tweaks.feature;
 
-import com.google.common.collect.ImmutableSet;
-
-import net.minecraft.init.Blocks;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
 import vazkii.quark.base.module.Feature;
+import vazkii.quark.management.feature.RightClickAddToShulkerBox;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BlastproofShulkerBoxes extends Feature {
 
+	public static List<ResourceLocation> shulkerBoxes;
+	public static boolean dropoffAnyShulkerBox;
+
 	@Override
-	public void preInit(FMLPreInitializationEvent event) {
-		ImmutableSet.of(Blocks.WHITE_SHULKER_BOX, Blocks.ORANGE_SHULKER_BOX, Blocks.MAGENTA_SHULKER_BOX, Blocks.LIGHT_BLUE_SHULKER_BOX,
-			Blocks.YELLOW_SHULKER_BOX, Blocks.LIME_SHULKER_BOX, Blocks.PINK_SHULKER_BOX, Blocks.GRAY_SHULKER_BOX,
-			Blocks.SILVER_SHULKER_BOX, Blocks.CYAN_SHULKER_BOX, Blocks.PURPLE_SHULKER_BOX, Blocks.BLUE_SHULKER_BOX,
-			Blocks.BROWN_SHULKER_BOX, Blocks.GREEN_SHULKER_BOX, Blocks.RED_SHULKER_BOX, Blocks.BLACK_SHULKER_BOX)
-		.forEach((b) -> b.setResistance(2000));
+	public void setupConfig() {
+		String[] shulkerArr = loadPropStringList("Shulker Boxes",
+				"Blocks which should be interpreted as Shulker Boxes.",
+				RightClickAddToShulkerBox.getBasicShulkerBoxes());
+		shulkerBoxes = Arrays.stream(shulkerArr).map(ResourceLocation::new).collect(Collectors.toList());
+
+		dropoffAnyShulkerBox = loadPropBool("Dropoff to Any Shulker Box",
+				"Allow anything with 'shulker_box' in its item identifier to be treated as a shulker box?", true);
+	}
+
+	@Override
+	public void init() {
+		for (Block block : Block.REGISTRY) {
+			if (RightClickAddToShulkerBox.isShulkerBox(block, shulkerBoxes, dropoffAnyShulkerBox))
+				block.setResistance(2000);
+		}
 	}
 	
 	@Override
