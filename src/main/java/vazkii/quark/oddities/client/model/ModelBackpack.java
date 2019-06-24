ad4b13818cd04bbbb5d2a5dd7d76fad9359d3ab6
yrsegal@gmail.com <yrsegal@gmail.com>
2019-06-25 00:01:34 +0300

    Fix z-fighting

--- src/main/java/vazkii/quark/oddities/client/model/ModelBackpack.java
+++ src/main/java/vazkii/quark/oddities/client/model/ModelBackpack.java
@@ -20,13 +20,13 @@ public class ModelBackpack extends ModelModArmor {
 
 		straps = new ModelRenderer(this, 24, 0);
 		straps.setRotationPoint(0.0F, 0.0F, 0.0F);
-		straps.addBox(-4.0F, 0.0F, -3.0F, 8, 8, 5, 0.0F);
+		straps.addBox(-4.0F, 0.05F, -3.0F, 8, 8, 5, 0.0F);
 		fitting = new ModelRenderer(this, 50, 0);
 		fitting.setRotationPoint(0.0F, 0.0F, 0.0F);
 		fitting.addBox(-1.0F, 3.0F, 6.0F, 2, 3, 1, 0.0F);
 		backpack = new ModelRenderer(this, 0, 0);
 		backpack.setRotationPoint(0.0F, 0.0F, 0.0F);
-		backpack.addBox(-4.0F, 0.05F, 2.0F, 8, 10, 4, 0.0F);
+		backpack.addBox(-4.0F, 0.0F, 2.0F, 8, 10, 4, 0.0F);
 //		lock = new ModelRenderer(this, 50, 4);
 //		lock.setRotationPoint(0.0F, 0.0F, 0.0F);
 //		lock.addBox(-2.0F, 4.0F, 6.0F, 4, 3, 2, 0.0F);
