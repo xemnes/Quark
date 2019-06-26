50d3df4a4cd8fe2dee1f7ad887f204faddf32d86
yrsegal@gmail.com <yrsegal@gmail.com>
2019-06-26 20:43:45 +0300

    paper wall is burny

--- src/main/java/vazkii/quark/decoration/block/BlockPaperWall.java
+++ src/main/java/vazkii/quark/decoration/block/BlockPaperWall.java
@@ -32,6 +32,16 @@ public class BlockPaperWall extends BlockQuarkPane {
 	}
 
 	@Override
+	public int getFlammability(IBlockAccess world, BlockPos pos, EnumFacing face) {
+		return 30;
+	}
+
+	@Override
+	public int getFireSpreadSpeed(IBlockAccess world, BlockPos pos, EnumFacing face) {
+		return 60;
+	}
+
+	@Override
 	public boolean canPlaceTorchOnTop(@Nonnull IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
 		return !state.getValue(NORTH) && !state.getValue(SOUTH) && !state.getValue(EAST) && !state.getValue(WEST);
 	}
