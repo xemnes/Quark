615834f60273e75c263b51d0b6c486e61f0830ef
yrsegal@gmail.com <yrsegal@gmail.com>
2019-06-26 22:21:09 +0300

    Poison potatos give feedback

--- src/main/java/vazkii/quark/misc/feature/PoisonPotatoUsage.java
+++ src/main/java/vazkii/quark/misc/feature/PoisonPotatoUsage.java
@@ -3,8 +3,8 @@ package vazkii.quark.misc.feature;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.init.Items;
-import net.minecraft.init.MobEffects;
-import net.minecraft.potion.PotionEffect;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -27,13 +27,16 @@ public class PoisonPotatoUsage extends Feature {
 			EntityAnimal animal = (EntityAnimal) event.getTarget();
 			if(animal.isChild() && !isEntityPoisoned(animal)) {
 				if(!event.getWorld().isRemote) {
+					animal.playSound(SoundEvents.ENTITY_GENERIC_EAT, 0.5f, 0.5f);
 					if(animal.world.rand.nextDouble() < chance) {
+						animal.world.spawnParticle(EnumParticleTypes.SPELL_MOB, animal.posX, animal.posY, animal.posZ, 0.2, 0.8, 0);
 						poisonEntity(animal);
-						animal.addPotionEffect(new PotionEffect(MobEffects.POISON, 60, 0));
-					}
+					} else
+						animal.world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, animal.posX, animal.posY, animal.posZ, 0, 0.1, 0);
 				} else event.getEntityPlayer().swingArm(event.getHand());
-				
-				event.getItemStack().shrink(1);
+
+				if (!event.getEntityPlayer().isCreative())
+					event.getItemStack().shrink(1);
 			}
 		}
 	}
