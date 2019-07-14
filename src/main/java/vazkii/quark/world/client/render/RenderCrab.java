726a556cd6027f5a4ce8be702b7ae4e577ba7fba
yrsegal@gmail.com <yrsegal@gmail.com>
2019-07-13 21:06:35 -0400

    crabs with render

--- src/main/java/vazkii/quark/world/client/render/RenderCrab.java
+++ src/main/java/vazkii/quark/world/client/render/RenderCrab.java
@@ -7,18 +7,20 @@ import net.minecraftforge.fml.client.registry.IRenderFactory;
 import vazkii.quark.world.client.model.ModelCrab;
 import vazkii.quark.world.entity.EntityCrab;
 
+import javax.annotation.Nonnull;
+
 public class RenderCrab extends RenderLiving<EntityCrab> {
 
-	private static final ResourceLocation TEXTURE = new ResourceLocation("quark", "textures/entity/archeologist.png");
+	private static final ResourceLocation TEXTURE = new ResourceLocation("quark", "textures/entity/crab.png");
 	
-	public static final IRenderFactory FACTORY = (RenderManager manager) -> new RenderCrab(manager);
+	public static final IRenderFactory<EntityCrab> FACTORY = RenderCrab::new;
 
-	public RenderCrab(RenderManager rendermanagerIn) {
-		super(rendermanagerIn, new ModelCrab(), 0.5F);
+	public RenderCrab(RenderManager render) {
+		super(render, new ModelCrab(), 0.5F);
 	}
 	
 	@Override
-    protected ResourceLocation getEntityTexture(EntityCrab entity) {
+    protected ResourceLocation getEntityTexture(@Nonnull EntityCrab entity) {
         return TEXTURE;
     }
 	
