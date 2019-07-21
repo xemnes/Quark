3ae97d65f473288b3a9a05aec8cde8dac8515ef5
yrsegal@gmail.com <yrsegal@gmail.com>
2019-07-21 14:27:56 -0400

    Make stonelings run even when they can't find where to run to

--- src/main/java/vazkii/quark/world/entity/ai/EntityAIRunAndPoof.java
+++ src/main/java/vazkii/quark/world/entity/ai/EntityAIRunAndPoof.java
@@ -11,50 +11,112 @@
 package vazkii.quark.world.entity.ai;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.ai.EntityAIAvoidEntity;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.RandomPositionGenerator;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.pathfinding.Path;
+import net.minecraft.pathfinding.PathNavigate;
+import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import vazkii.quark.base.sounds.QuarkSounds;
 import vazkii.quark.world.entity.EntityStoneling;
 
-public class EntityAIRunAndPoof<T extends Entity> extends EntityAIAvoidEntity<T> {
-	
-	private final EntityStoneling stoneling;
-	
-	public EntityAIRunAndPoof(EntityStoneling stoneling, Class<T> classToAvoidIn, float avoidDistanceIn, double farSpeedIn, double nearSpeedIn) {
-		super(stoneling, classToAvoidIn, avoidDistanceIn, farSpeedIn, nearSpeedIn);
-		this.stoneling = stoneling;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class EntityAIRunAndPoof<T extends Entity> extends EntityAIBase {
+
+	private final Predicate<Entity> canBeSeenSelector;
+	protected EntityStoneling entity;
+	private final double farSpeed;
+	private final double nearSpeed;
+	protected T closestLivingEntity;
+	private final float avoidDistance;
+	private Path path;
+	private final PathNavigate navigation;
+	private final Class<T> classToAvoid;
+	private final java.util.function.Predicate<T> avoidTargetSelector;
+
+	public EntityAIRunAndPoof(EntityStoneling entity, Class<T> classToAvoid, float avoidDistance, double farSpeed, double nearSpeed) {
+		this(entity, classToAvoid, t -> true, avoidDistance, farSpeed, nearSpeed);
+	}
+
+	public EntityAIRunAndPoof(EntityStoneling entity, Class<T> classToAvoid, java.util.function.Predicate<T> avoidTargetSelector, float avoidDistance, double farSpeed, double nearSpeed) {
+		this.canBeSeenSelector = target -> target != null && target.isEntityAlive() && entity.getEntitySenses().canSee(target) && !entity.isOnSameTeam(target);
+		this.entity = entity;
+		this.classToAvoid = classToAvoid;
+		this.avoidTargetSelector = avoidTargetSelector;
+		this.avoidDistance = avoidDistance;
+		this.farSpeed = farSpeed;
+		this.nearSpeed = nearSpeed;
+		this.navigation = entity.getNavigator();
+		this.setMutexBits(1);
 	}
 
 	@Override
 	public boolean shouldExecute() {
-		return !stoneling.isPlayerMade() && super.shouldExecute();
+		if (entity.isPlayerMade())
+			return false;
+
+		List<T> entities = this.entity.world.getEntitiesWithinAABB(this.classToAvoid, this.entity.getEntityBoundingBox().grow(this.avoidDistance, 3.0D, this.avoidDistance),
+				(entity) -> EntitySelectors.CAN_AI_TARGET.apply(entity) && this.canBeSeenSelector.test(entity) && this.avoidTargetSelector.test(entity));
+
+		if (entities.isEmpty())
+			return false;
+		else {
+			this.closestLivingEntity = entities.get(0);
+			Vec3d target = RandomPositionGenerator.findRandomTargetBlockAwayFrom(this.entity, 16, 7, this.closestLivingEntity.getPositionVector());
+
+			if (target == null)
+				target = entity.getPositionVector();
+
+			if (this.closestLivingEntity.getDistanceSq(target.x, target.y, target.z) < this.closestLivingEntity.getDistanceSq(this.entity))
+				return false;
+			else {
+				this.path = this.navigation.getPathToXYZ(target.x, target.y, target.z);
+				return this.path != null;
+			}
+		}
 	}
-	
+
+	@Override
+	public boolean shouldContinueExecuting() {
+		return !this.navigation.noPath();
+	}
+
 	@Override
 	public void startExecuting() {
-		super.startExecuting();
+		this.navigation.setPath(this.path, this.farSpeed);
 		entity.world.playSound(null, entity.posX, entity.posY, entity.posZ, QuarkSounds.ENTITY_STONELING_MEEP, SoundCategory.NEUTRAL, 1.0F, 1.0F);
 	}
 
 	@Override
 	public void resetTask() {
-		super.resetTask();
+		this.closestLivingEntity = null;
 
 		World world = entity.world;
 
-		if(world instanceof WorldServer) {
+		if (world instanceof WorldServer) {
 			WorldServer ws = (WorldServer) world;
 			ws.spawnParticle(EnumParticleTypes.CLOUD, entity.posX, entity.posY, entity.posZ, 40, 0.5, 0.5, 0.5, 0.1);
 			ws.spawnParticle(EnumParticleTypes.EXPLOSION_NORMAL, entity.posX, entity.posY, entity.posZ, 20, 0.5, 0.5, 0.5, 0);
 		}
-		for (Entity passenger : entity.getRecursivePassengers()) {
+		for (Entity passenger : entity.getRecursivePassengers())
 			if (!(passenger instanceof EntityPlayer))
 				passenger.setDead();
-		}
 		entity.setDead();
 	}
+
+	@Override
+	public void updateTask() {
+		if (this.entity.getDistanceSq(this.closestLivingEntity) < 49.0D)
+			this.entity.getNavigator().setSpeed(this.nearSpeed);
+		else
+			this.entity.getNavigator().setSpeed(this.farSpeed);
+	}
+
 }
