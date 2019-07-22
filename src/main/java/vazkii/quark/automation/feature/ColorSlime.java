902eeeec62842b6be9b424b7c63135b51e855aff
Vasco Lavos <vazkii@hotmail.com>
2019-07-22 12:31:48 +0100

    Use a multi recipe to avoid a race condition

--- src/main/java/vazkii/quark/automation/feature/ColorSlime.java
+++ src/main/java/vazkii/quark/automation/feature/ColorSlime.java
@@ -3,8 +3,10 @@ package vazkii.quark.automation.feature;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.oredict.OreDictionary;
+import vazkii.arl.recipe.MultiRecipe;
 import vazkii.arl.recipe.RecipeHandler;
 import vazkii.arl.util.ProxyRegistry;
 import vazkii.quark.automation.block.BlockColorSlime;
@@ -16,6 +18,8 @@ public class ColorSlime extends Feature {
 	
 	boolean renameVanillaSlime;
 	
+	private MultiRecipe multiRecipe;
+	
 	@Override
 	public void setupConfig() {
 		renameVanillaSlime = loadPropBool("Rename Vanilla Slime", "Set to false to not rename vanilla slime to Green Slime Block", true);
@@ -25,13 +29,9 @@ public class ColorSlime extends Feature {
 	public void preInit(FMLPreInitializationEvent event) {
 		color_slime = new BlockColorSlime();
 
-		RecipeHandler.addShapelessOreDictRecipe(new ItemStack(Blocks.SLIME_BLOCK), "blockSlime", "dyeGreen");
-		RecipeHandler.addShapelessOreDictRecipe(ProxyRegistry.newStack(color_slime, 1, 0), "blockSlime", "dyeRed");
-		RecipeHandler.addShapelessOreDictRecipe(ProxyRegistry.newStack(color_slime, 1, 1), "blockSlime", "dyeBlue");
-		
-		RecipeHandler.addShapelessOreDictRecipe(ProxyRegistry.newStack(color_slime, 2, 2), ProxyRegistry.newStack(Blocks.SLIME_BLOCK), ProxyRegistry.newStack(color_slime, 1, 1));
-		RecipeHandler.addShapelessOreDictRecipe(ProxyRegistry.newStack(color_slime, 2, 3), ProxyRegistry.newStack(color_slime, 1, 0), ProxyRegistry.newStack(color_slime, 1, 1));
-		RecipeHandler.addShapelessOreDictRecipe(ProxyRegistry.newStack(color_slime, 2, 4), ProxyRegistry.newStack(color_slime, 1, 0), ProxyRegistry.newStack(Blocks.SLIME_BLOCK));
+		// we use a MultiRecipe to avoid a race condition with the slime block not being registered in time
+		// (not sure why it happens but it happens!)
+		multiRecipe = new MultiRecipe(new ResourceLocation("quark", "slime_block_recipes"));
 
 		addOreDict("blockSlime", ProxyRegistry.newStack(color_slime, 1, OreDictionary.WILDCARD_VALUE));
 	}
@@ -40,6 +40,17 @@ public class ColorSlime extends Feature {
 	public void postPreInit() {
 		if(renameVanillaSlime)
 			Blocks.SLIME_BLOCK.setTranslationKey("green_slime_block");
+	}
+	
+	@Override
+	public void init() {
+		RecipeHandler.addShapelessOreDictRecipe(multiRecipe, new ItemStack(Blocks.SLIME_BLOCK), "blockSlime", "dyeGreen");
+		RecipeHandler.addShapelessOreDictRecipe(multiRecipe, ProxyRegistry.newStack(color_slime, 1, 0), "blockSlime", "dyeRed");
+		RecipeHandler.addShapelessOreDictRecipe(multiRecipe, ProxyRegistry.newStack(color_slime, 1, 1), "blockSlime", "dyeBlue");
+		
+		RecipeHandler.addShapelessOreDictRecipe(multiRecipe, ProxyRegistry.newStack(color_slime, 2, 2), ProxyRegistry.newStack(Blocks.SLIME_BLOCK), ProxyRegistry.newStack(color_slime, 1, 1));
+		RecipeHandler.addShapelessOreDictRecipe(multiRecipe, ProxyRegistry.newStack(color_slime, 2, 3), ProxyRegistry.newStack(color_slime, 1, 0), ProxyRegistry.newStack(color_slime, 1, 1));
+		RecipeHandler.addShapelessOreDictRecipe(multiRecipe, ProxyRegistry.newStack(color_slime, 2, 4), ProxyRegistry.newStack(color_slime, 1, 0), ProxyRegistry.newStack(Blocks.SLIME_BLOCK));
 	}
 	
 	@Override
