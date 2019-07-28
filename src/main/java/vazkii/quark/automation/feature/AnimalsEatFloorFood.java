3d8bd1fe027eaf6de9d5eddd4df98468668ad788
yrsegal@gmail.com <yrsegal@gmail.com>
2019-07-28 15:16:42 -0400

    Fix floor food, again, again

--- src/main/java/vazkii/quark/automation/feature/AnimalsEatFloorFood.java
+++ src/main/java/vazkii/quark/automation/feature/AnimalsEatFloorFood.java
@@ -1,70 +1,27 @@
 package vazkii.quark.automation.feature;
 
-import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityWolf;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.management.PlayerChunkMapEntry;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import vazkii.quark.base.module.Feature;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class AnimalsEatFloorFood extends Feature {
 
-	// From WorldEntitySpawner
-	private static final int MOB_COUNT_DIV = 17 * 17;
-
-	private static final Set<ChunkPos> eligibleChunksForSpawning = new HashSet<>();
-
-	public static int maxCreaturesPerChunkArea;
+	public static double maxDensity;
 
 	@Override
 	public void setupConfig() {
-		maxCreaturesPerChunkArea = loadPropInt("Maximum entities per chunk area", "Prevents entities from proliferating infinitely. Set to 0 or less to disable checking.", 30);
-	}
-
-	private int getSpawnAllowedChunks(EntityAnimal animal, WorldServer world) {
-		eligibleChunksForSpawning.clear();
-		int chunks = 0;
-
-		for (EntityPlayer entityplayer : world.playerEntities) {
-			if (!entityplayer.isSpectator() && animal.getDistanceSq(entityplayer) < 4096) {
-				int chunkX = MathHelper.floor(entityplayer.posX / 16.0D);
-				int chunkZ = MathHelper.floor(entityplayer.posZ / 16.0D);
-
-				for (int x = -8; x <= 8; ++x) {
-					for (int z = -8; z <= 8; ++z) {
-						boolean chunkEdge = x == -8 || x == 8 || z == -8 || z == 8;
-						ChunkPos chunkPos = new ChunkPos(x + chunkX, z + chunkZ);
-
-						if (!eligibleChunksForSpawning.contains(chunkPos)) {
-							chunks++;
-
-							if (!chunkEdge && world.getWorldBorder().contains(chunkPos)) {
-								PlayerChunkMapEntry chunkEntry = world.getPlayerChunkMap().getEntry(chunkPos.x, chunkPos.z);
-
-								if (chunkEntry != null && chunkEntry.isSentToPlayers())
-									eligibleChunksForSpawning.add(chunkPos);
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return chunks;
+		maxDensity = loadPropDouble("Maximum entity density", "Prevents entities from proliferating infinitely. Set to 0 or less to disable checking.", 2);
 	}
 
 	@SubscribeEvent
@@ -74,12 +31,22 @@ public class AnimalsEatFloorFood extends Feature {
 			if(animal.getGrowingAge() == 0 && animal.ticksExisted % 20 == 0 && !animal.isInLove() && !animal.isDead) {
 				double range = 2;
 
-				if (maxCreaturesPerChunkArea > 0) {
-					int count = animal.world.countEntities(EnumCreatureType.CREATURE, true);
-					int max = maxCreaturesPerChunkArea * getSpawnAllowedChunks(animal, (WorldServer) animal.world) / MOB_COUNT_DIV;
 
-					if (count > max)
-						return;
+				double density = 0;
+
+				if (maxDensity > 0) {
+					List<Entity> animals = animal.world.getEntitiesWithinAABBExcludingEntity(animal, animal.getEntityBoundingBox().grow(10));
+
+					for (Entity other : animals) {
+						double dSq = other.getDistanceSq(animal);
+
+						if (dSq <= 100) {
+							density += 1 / dSq;
+
+							if (density > maxDensity)
+								return;
+						}
+					}
 				}
 
 				List<EntityItem> nearbyFood = animal.getEntityWorld().getEntitiesWithinAABB(EntityItem.class, animal.getEntityBoundingBox().expand(range, 0, range),
