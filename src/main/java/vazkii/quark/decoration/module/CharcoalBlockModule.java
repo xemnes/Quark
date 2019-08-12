62a3dabf159929b9258c95ab670d2dc9b434caf6
Vasco Lavos <vazkii@hotmail.com>
2019-08-12 05:11:41 +0100

    Make charcoal burn

--- src/main/java/vazkii/quark/decoration/module/CharcoalBlockModule.java
+++ src/main/java/vazkii/quark/decoration/module/CharcoalBlockModule.java
@@ -1,19 +1,33 @@
 package vazkii.quark.decoration.module;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import vazkii.quark.base.moduleloader.Config;
 import vazkii.quark.base.moduleloader.LoadModule;
 import vazkii.quark.base.moduleloader.Module;
 import vazkii.quark.base.moduleloader.ModuleCategory;
+import vazkii.quark.base.moduleloader.SubscriptionTarget;
 import vazkii.quark.decoration.block.CharcoalBlock;
 
-@LoadModule(category = ModuleCategory.DECORATION)
+@LoadModule(category = ModuleCategory.DECORATION, subscriptions = SubscriptionTarget.BOTH_SIDES)
 public class CharcoalBlockModule extends Module {
 
 	@Config public static boolean burnsForever = true; 
+	@Config public static int burnTime = 16000;
+	
+	private Block charcoal_block;
 	
 	@Override
 	public void start() {
-		new CharcoalBlock(this);
+		charcoal_block = new CharcoalBlock(this);
+	}
+
+	@SubscribeEvent
+	public void onFurnaceFuelEvent(FurnaceFuelBurnTimeEvent event) {
+		if(event.getItemStack().getItem() == charcoal_block.asItem())
+			event.setBurnTime(burnTime);
 	}
 	
 }
