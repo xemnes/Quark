aeb9d11e16a24ad849e59513b9bf775ad47339f5
Vasco Lavos <vazkii@hotmail.com>
2019-08-15 13:44:22 +0100

    Permafrost biome

---
+++ src/main/java/vazkii/quark/world/module/underground/PermafrostUndergroundBiomeModule.java
@@ -0,0 +1,38 @@
+package vazkii.quark.world.module.underground;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.BiomeDictionary.Type;
+import vazkii.quark.base.block.QuarkBlock;
+import vazkii.quark.base.handler.VariantHandler;
+import vazkii.quark.base.moduleloader.LoadModule;
+import vazkii.quark.base.moduleloader.ModuleCategory;
+import vazkii.quark.world.config.UndergroundBiomeConfig;
+import vazkii.quark.world.gen.underground.PermafrostUndergroundBiome;
+
+@LoadModule(category = ModuleCategory.WORLD)
+public class PermafrostUndergroundBiomeModule extends UndergroundBiomeModule {
+
+	public static Block permafrost;
+	
+	@Override
+	public void start() {
+		permafrost = new QuarkBlock("permafrost", this, ItemGroup.BUILDING_BLOCKS, 
+				Block.Properties.create(Material.ROCK, MaterialColor.LIGHT_BLUE)
+				.hardnessAndResistance(1.5F, 10F)
+				.sound(SoundType.STONE));
+		
+		VariantHandler.addSlabStairsWall(new QuarkBlock("permafrost_bricks", this, ItemGroup.BUILDING_BLOCKS, Block.Properties.from(permafrost)));
+		
+		super.start();
+	}
+	
+	@Override
+	protected UndergroundBiomeConfig getBiomeConfig() {
+		return new UndergroundBiomeConfig(new PermafrostUndergroundBiome(), 80, Type.COLD);
+	}
+
+}
