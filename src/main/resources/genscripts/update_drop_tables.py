55ba522046b6890fd24efe37ddfe6a4e311ac989
Vasco Lavos <vazkii@hotmail.com>
2019-08-15 15:10:24 +0100

    Update slab drop tables

--- src/main/resources/genscripts/update_drop_tables.py
+++ src/main/resources/genscripts/update_drop_tables.py
@@ -1,12 +1,18 @@
 from jsongen import *
 import os
 
-copy_targets = []
+generic_targets = []
+slab_targets = []
 
 for file in os.listdir('../assets/quark/blockstates'):
 	if '.json' in file:
 		test = '../data/quark/loot_tables/blocks/' + file
 		if not os.path.exists(test):
-			copy_targets.append(file.replace('.json', ''))
+			block_name = file.replace('.json', '')
+			if '_slab' in block_name:
+				slab_targets.append(block_name)
+			else: 
+				generic_targets.append(block_name)
 
-foreach_arg_array(0, copy_targets, [('drop_table_generic.json', 'data/{modid}/loot_tables/blocks/{name}.json')], copy_callback)
+foreach_arg_array(0, generic_targets, [('drop_table_generic.json', 'data/{modid}/loot_tables/blocks/{name}.json')], copy_callback)
+foreach_arg_array(0, slab_targets, [('drop_table_slab.json', 'data/{modid}/loot_tables/blocks/{name}.json')], copy_callback)
