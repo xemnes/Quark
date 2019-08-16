a55e16df576d472feec7064ec6fe17f2d955f7a1
Vasco Lavos <vazkii@hotmail.com>
2019-08-16 09:24:52 +0100

    Shingles

---
+++ src/main/java/vazkii/quark/building/module/ShinglesModule.java
@@ -0,0 +1,22 @@
+package vazkii.quark.building.module;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.DyeColor;
+import net.minecraft.item.ItemGroup;
+import vazkii.quark.base.block.QuarkBlock;
+import vazkii.quark.base.handler.VariantHandler;
+import vazkii.quark.base.module.LoadModule;
+import vazkii.quark.base.module.Module;
+import vazkii.quark.base.module.ModuleCategory;
+
+@LoadModule(category = ModuleCategory.BUILDING)
+public class ShinglesModule extends Module {
+
+	@Override
+	public void start() {
+		for(DyeColor dye : DyeColor.values())
+			VariantHandler.addSlabAndStairs(new QuarkBlock(dye.getName() + "_shingles", this, ItemGroup.BUILDING_BLOCKS, Block.Properties.from(Blocks.TERRACOTTA)));
+	}
+
+}
