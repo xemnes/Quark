b514d1ef30e4f9cd84d06e60640ba672f03a986c
Vasco Lavos <vazkii@hotmail.com>
2019-08-21 05:27:43 +0100

    Vertical stained planks

---
+++ src/main/resources/genscripts/vertical_stained_planks.py
@@ -0,0 +1,7 @@
+from jsongen import *
+
+copy([
+	('unique/block_model_vertical_stained_planks.json', 'assets/{modid}/models/block/vertical_{name}_stained_planks.json'),
+	('unique/recipe_vertical_stained_planks.json', 'data/{modid}/recipes/building/crafting/vertical_{name}_stained_planks.json'),
+	('unique/recipe_vertical_stained_planks_revert.json', 'data/{modid}/recipes/building/crafting/vertical_{name}_stained_planks_revert.json')
+])
