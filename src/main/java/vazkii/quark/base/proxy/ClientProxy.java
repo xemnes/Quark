26466156f9388adfd5c1d8ad140f765ba2ab54d8
Vasco Lavos <vazkii@hotmail.com>
2019-08-21 15:59:35 +0100

    Optimize resource overrides

--- src/main/java/vazkii/quark/base/proxy/ClientProxy.java
+++ src/main/java/vazkii/quark/base/proxy/ClientProxy.java
@@ -1,13 +1,24 @@
 package vazkii.quark.base.proxy;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.IAsyncReloader;
+import net.minecraft.resources.IReloadableResourceManager;
+import net.minecraft.resources.IResourcePack;
+import net.minecraft.resources.ResourcePackInfo;
+import net.minecraft.util.Unit;
+import net.minecraft.util.Util;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.StartupMessageManager;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.resource.ReloadRequirements;
+import net.minecraftforge.resource.SelectiveReloadStateHandler;
 import net.minecraftforge.resource.VanillaResourceType;
 import vazkii.quark.base.handler.ContributorRewardHandler;
 import vazkii.quark.base.handler.ResourceProxy;
@@ -18,44 +29,52 @@ public class ClientProxy extends CommonProxy {
 	@Override
 	public void start() {
 		ResourceProxy.init();
-		
+
 		super.start();
 	}
-	
+
 	@Override
 	public void registerListeners(IEventBus bus) {
 		super.registerListeners(bus);
-		
+
 		bus.addListener(this::clientSetup);
 	}
-	
+
 	public void clientSetup(FMLClientSetupEvent event) {
 		ModuleLoader.INSTANCE.clientSetup();
 		ContributorRewardHandler.setupClient();
 	}
-	
+
 	@Override
 	public void loadComplete(FMLLoadCompleteEvent event) {
 		super.loadComplete(event);
 		
-		if(ResourceProxy.instance().hasAny())
-			ForgeHooksClient.refreshResources(Minecraft.getInstance(), VanillaResourceType.MODELS);
+		if(ResourceProxy.instance().hasAny()) {
+			StartupMessageManager.addModMessage("Quark: Applying vanilla resource overrides...");
+			Minecraft mc = Minecraft.getInstance();
+			List<IResourcePack> packs = mc.getResourcePackList().getEnabledPacks().stream().map(ResourcePackInfo::getResourcePack).collect(Collectors.toList());
+			
+			SelectiveReloadStateHandler.INSTANCE.beginReload(ReloadRequirements.include(VanillaResourceType.MODELS));
+			IAsyncReloader async = ((IReloadableResourceManager) mc.getResourceManager()).reloadResources(Util.getServerExecutor(), mc, CompletableFuture.completedFuture(Unit.INSTANCE), packs);
+			async.join();
+			SelectiveReloadStateHandler.INSTANCE.endReload();
+		}
 	}
-	
+
 	@Override	
 	public void handleQuarkConfigChange() {
 		super.handleQuarkConfigChange();
-		
+
 		Minecraft mc = Minecraft.getInstance();
 		if(mc.isSingleplayer()) {
-	        mc.player.sendMessage(new TranslationTextComponent("commands.reload.success"));
-	        mc.getIntegratedServer().reload();
+			mc.player.sendMessage(new TranslationTextComponent("commands.reload.success"));
+			mc.getIntegratedServer().reload();
 		}
 	}
-	
+
 	@Override
 	public void addResourceOverride(String type, String path, String file, Supplier<Boolean> isEnabled) {
 		ResourceProxy.instance().addResource(type, path, file, isEnabled);
 	}
-	
+
 }
