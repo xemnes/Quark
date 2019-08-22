7fddb63a7c6bf309e078c197c96e505ea9e7a31a
Vasco Lavos <vazkii@hotmail.com>
2019-08-22 20:37:03 +0100

    q button

--- src/main/java/vazkii/quark/base/handler/GeneralConfig.java
+++ src/main/java/vazkii/quark/base/handler/GeneralConfig.java
@@ -6,13 +6,19 @@ public class GeneralConfig {
 	
 	public static final GeneralConfig INSTANCE = new GeneralConfig();
 	
+	@Config(name = "Enable 'q' Button")
+	public static boolean enableQButton = true;
+	
+	@Config(name = "'q' Button on the Right")
+	public static boolean qButtonOnRight = false;
+	
+	@Config public static boolean useAntiOverlap = true;
+	
 	@Config(name = "Use Piston Logic Replacement",
 			description = "Quark replaces the Piston logic to allow for its piston features to work. If you're having troubles, try turning this off.")
 	public static boolean usePistonLogicRepl = true;
 	
 	@Config public static int pistonPushLimit = 12;
-	
-	@Config public static boolean useAntiOverlap = true;
 	
 	@Config(description = "Quark messes with the Stonecutter to allow any item that can be used in it to be shift clicked in. Set this to false to turn it off.")
 	public static boolean hackStonecutterShiftClick = true;
