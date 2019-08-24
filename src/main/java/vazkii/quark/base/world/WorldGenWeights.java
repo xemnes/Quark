78bf01a95fbcd5723e0190d2ac512450dc3ef9fb
Vasco Lavos <vazkii@hotmail.com>
2019-08-24 19:06:35 +0100

    Generating biotite ore

--- src/main/java/vazkii/quark/base/world/WorldGenWeights.java
+++ src/main/java/vazkii/quark/base/world/WorldGenWeights.java
@@ -4,6 +4,7 @@ public class WorldGenWeights {
 
 	// UNDERGROUND_ORES
 	public static final int CLAY = 0;
+	public static final int BIOTITE = 1;
 	
 	// UNDERGROUND_DECORATION
 	public static final int UNDERGROUND_BIOMES = 0;
