6bc00ffbf43dd7e20715e8f8c7540811eb201e74
Vasco Lavos <vazkii@hotmail.com>
2019-08-26 18:55:43 +0100

    Fix up speleothems, gen missing

--- src/main/resources/genscripts/update_drop_tables.py
+++ src/main/resources/genscripts/update_drop_tables.py
@@ -3,16 +3,20 @@ import os
 
 generic_targets = []
 slab_targets = []
+silk_targets = []
 
 for file in os.listdir('../assets/quark/blockstates'):
 	if '.json' in file:
 		test = '../data/quark/loot_tables/blocks/' + file
 		if not os.path.exists(test):
 			block_name = file.replace('.json', '')
-			if '_slab' in block_name:
+			if '_speleothem' in block_name:
+				silk_targets.append(block_name)
+			elif '_slab' in block_name:
 				slab_targets.append(block_name)
 			else: 
 				generic_targets.append(block_name)
 
 foreach_arg_array(0, generic_targets, [('drop_table_generic.json', 'data/{modid}/loot_tables/blocks/{name}.json')], copy_callback)
 foreach_arg_array(0, slab_targets, [('drop_table_slab.json', 'data/{modid}/loot_tables/blocks/{name}.json')], copy_callback)
+foreach_arg_array(0, silk_targets, [('drop_table_silk.json', 'data/{modid}/loot_tables/blocks/{name}.json')], copy_callback)
