b73b995e5ecf6013cbcdcdc5d327d6db10a97a06
Vasco Lavos <vazkii@hotmail.com>
2019-10-11 17:34:42 +0100

    Variant animal textures

---
+++ src/main/java/vazkii/quark/client/render/variant/VariantChickenRenderer.java
@@ -0,0 +1,21 @@
+package vazkii.quark.client.render.variant;
+
+import net.minecraft.client.renderer.entity.ChickenRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.util.ResourceLocation;
+import vazkii.quark.client.module.VariantAnimalTexturesModule;
+import vazkii.quark.client.module.VariantAnimalTexturesModule.VariantTextureType;
+
+public class VariantChickenRenderer extends ChickenRenderer {
+
+	public VariantChickenRenderer(EntityRendererManager renderManagerIn) {
+		super(renderManagerIn);
+	}
+	
+	@Override
+	protected ResourceLocation getEntityTexture(ChickenEntity entity) {
+		return VariantAnimalTexturesModule.getTextureOrShiny(entity, VariantTextureType.CHICKEN, VariantAnimalTexturesModule.enableChicken);
+	}
+
+}
---
+++ src/main/java/vazkii/quark/client/render/variant/VariantRabbitRenderer.java
@@ -0,0 +1,21 @@
+package vazkii.quark.client.render.variant;
+
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.RabbitRenderer;
+import net.minecraft.entity.passive.RabbitEntity;
+import net.minecraft.util.ResourceLocation;
+import vazkii.quark.client.module.VariantAnimalTexturesModule;
+import vazkii.quark.client.module.VariantAnimalTexturesModule.VariantTextureType;
+
+public class VariantRabbitRenderer extends RabbitRenderer {
+
+	public VariantRabbitRenderer(EntityRendererManager renderManagerIn) {
+		super(renderManagerIn);
+	}
+	
+	@Override
+	protected ResourceLocation getEntityTexture(RabbitEntity entity) {
+		return VariantAnimalTexturesModule.getTextureOrShiny(entity, VariantTextureType.RABBIT, () -> super.getEntityTexture(entity));
+	}
+
+}
