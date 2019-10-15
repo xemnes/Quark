fb2b51c99743f7d04e85034f91a8d8f00157368b
Vasco Lavos <vazkii@hotmail.com>
2019-10-15 20:21:22 +0100

    Stone variant recipes

--- src/main/resources/genscripts/stone_variant_recipes.py
+++ src/main/resources/genscripts/stone_variant_recipes.py
@@ -4,10 +4,10 @@ import re
 # Use category=... and flag=... to set the flags for this one
 
 copy([
-	('unique/recipe_stone_bricks.json', 'data/{modid}/recipes/world/crafting/{name}_bricks.json'),
-	('unique/recipe_stone_chiseled.json', 'data/{modid}/recipes/world/crafting/chiseled_{name}_bricks.json'),
-	('unique/recipe_stone_pavement.json', 'data/{modid}/recipes/world/crafting/{name}_pavement.json'),
-	('unique/recipe_stone_pillar.json', 'data/{modid}/recipes/world/crafting/{name}_pillar.json'),
+	('unique/recipe_stone_bricks.json', 'data/{modid}/recipes/building/crafting/stonevariants/{name}_bricks.json'),
+	('unique/recipe_stone_chiseled.json', 'data/{modid}/recipes/building/crafting/stonevariants/chiseled_{name}_bricks.json'),
+	('unique/recipe_stone_pavement.json', 'data/{modid}/recipes/building/crafting/stonevariants/{name}_pavement.json'),
+	('unique/recipe_stone_pillar.json', 'data/{modid}/recipes/building/crafting/stonevariants/{name}_pillar.json'),
 ])
 
 import update_drop_tables
