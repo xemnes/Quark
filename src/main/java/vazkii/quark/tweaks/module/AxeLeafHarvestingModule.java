802cab70550d99808783cfbf3b9e54abecbe12e7
Vasco Lavos <vazkii@hotmail.com>
2019-10-18 17:21:50 +0100

    Fix #1550

--- src/main/java/vazkii/quark/tweaks/module/AxeLeafHarvestingModule.java
+++ src/main/java/vazkii/quark/tweaks/module/AxeLeafHarvestingModule.java
@@ -1,6 +1,5 @@
 package vazkii.quark.tweaks.module;
 
-import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
@@ -18,7 +17,7 @@ public class AxeLeafHarvestingModule extends Module {
 	public void calcBreakSpeed(BreakSpeed event) {
 		ItemStack stack = event.getPlayer().getHeldItem(Hand.MAIN_HAND);
 		if(stack.getItem().getToolTypes(stack).contains(ToolType.AXE) && event.getState().getMaterial() == Material.LEAVES)
-			event.setNewSpeed(stack.getItem().getDestroySpeed(stack, Blocks.OAK_PLANKS.getDefaultState()));
+			event.setNewSpeed(100F);
 	}	
 
 }
