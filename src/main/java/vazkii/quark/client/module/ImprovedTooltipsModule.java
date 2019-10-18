0fb4a05756da03d8604973cd0afe93d2818f47cb
yrsegal@gmail.com <yrsegal@gmail.com>
2019-10-18 09:55:22 -0400

    add tridents and crossbows to default enchanting stacks

--- src/main/java/vazkii/quark/client/module/ImprovedTooltipsModule.java
+++ src/main/java/vazkii/quark/client/module/ImprovedTooltipsModule.java
@@ -48,7 +48,7 @@ public class ImprovedTooltipsModule extends Module {
     @Config
     public static List<String> enchantingStacks = Lists.newArrayList("minecraft:diamond_sword", "minecraft:diamond_pickaxe", "minecraft:diamond_shovel", "minecraft:diamond_axe", "minecraft:diamond_hoe",
             "minecraft:diamond_helmet", "minecraft:diamond_chestplate", "minecraft:diamond_leggings", "minecraft:diamond_boots",
-            "minecraft:shears", "minecraft:bow", "minecraft:fishing_rod", "minecraft:elytra", "quark:pickarang");
+            "minecraft:shears", "minecraft:bow", "minecraft:fishing_rod", "minecraft:crossbow", "minecraft:trident", "minecraft:elytra", "quark:pickarang");
 
     @Config(description = "A list of additional stacks to display on each enchantment\n"
             + "The format is as follows:\n"
