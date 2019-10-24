af3e4729c986cc1e05f4cc9ee5d184eb0e06bd9c
Vasco Lavos <vazkii@hotmail.com>
2019-10-24 04:20:23 +0100

    fuck

---
+++ src/main/java/vazkii/quark/building/module/MoreStoneVariantsModule.java
@@ -0,0 +1,60 @@
+package vazkii.quark.building.module;
+
+import java.util.function.BooleanSupplier;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.ItemGroup;
+import vazkii.quark.base.block.QuarkBlock;
+import vazkii.quark.base.block.QuarkPillarBlock;
+import vazkii.quark.base.handler.VariantHandler;
+import vazkii.quark.base.module.Config;
+import vazkii.quark.base.module.ConfigFlagManager;
+import vazkii.quark.base.module.LoadModule;
+import vazkii.quark.base.module.Module;
+import vazkii.quark.base.module.ModuleCategory;
+import vazkii.quark.base.module.ModuleLoader;
+import vazkii.quark.world.module.NewStoneTypesModule;
+
+@LoadModule(category = ModuleCategory.BUILDING)
+public class MoreStoneVariantsModule extends Module {
+
+	@Config(flag = "stone_bricks") public boolean enableBricks = true;
+	@Config(flag = "stone_chiseled") public boolean enableChiseledBricks = true;
+	@Config(flag = "stone_pavement") public boolean enablePavement = true;
+	@Config(flag = "stone_pillar") public boolean enablePillar = true;
+	
+	@Override
+	public void construct() {
+		BooleanSupplier _true = () -> true;
+		add("granite", MaterialColor.DIRT, _true);
+		add("diorite", MaterialColor.QUARTZ, _true);
+		add("andesite", MaterialColor.STONE, _true);
+		
+		add("marble", MaterialColor.QUARTZ, () -> ModuleLoader.INSTANCE.isModuleEnabled(NewStoneTypesModule.class) && NewStoneTypesModule.enableMarble);
+		add("limestone", MaterialColor.STONE, () -> ModuleLoader.INSTANCE.isModuleEnabled(NewStoneTypesModule.class) && NewStoneTypesModule.enableLimestone);
+		add("jasper", MaterialColor.RED_TERRACOTTA, () -> ModuleLoader.INSTANCE.isModuleEnabled(NewStoneTypesModule.class) && NewStoneTypesModule.enableJasper);
+		add("slate", MaterialColor.ICE, () -> ModuleLoader.INSTANCE.isModuleEnabled(NewStoneTypesModule.class) && NewStoneTypesModule.enableSlate);
+		add("basalt", MaterialColor.BLACK, () -> ModuleLoader.INSTANCE.isModuleEnabled(NewStoneTypesModule.class) && NewStoneTypesModule.enableBasalt);
+	}
+	
+	@Override
+	public void pushFlags(ConfigFlagManager manager) {
+		manager.putFlag(this, "granite", true);
+		manager.putFlag(this, "diorite", true);
+		manager.putFlag(this, "andesite", true);
+	}
+	
+	private void add(String name, MaterialColor color, BooleanSupplier cond) {
+		Block.Properties props = Block.Properties.create(Material.ROCK, color).hardnessAndResistance(1.5F, 6.0F);
+		
+		QuarkBlock bricks = new QuarkBlock(name + "_bricks", this, ItemGroup.BUILDING_BLOCKS, props).setCondition(() -> cond.getAsBoolean() && enableBricks);
+		VariantHandler.addSlabStairsWall(bricks);
+		
+		new QuarkBlock("chiseled_" + name + "_bricks", this, ItemGroup.BUILDING_BLOCKS, props).setCondition(() -> cond.getAsBoolean() && enableBricks && enableChiseledBricks);
+		new QuarkBlock(name + "_pavement", this, ItemGroup.BUILDING_BLOCKS, props).setCondition(() -> cond.getAsBoolean() && enablePavement);
+		new QuarkPillarBlock(name + "_pillar", this, ItemGroup.BUILDING_BLOCKS, props).setCondition(() -> cond.getAsBoolean() && enablePillar);
+	}
+	
+}
---
+++ src/main/java/vazkii/quark/building/module/VerticalSlabsModule.java
@@ -0,0 +1,29 @@
+package vazkii.quark.building.module;
+
+import com.google.common.collect.ImmutableSet;
+
+import net.minecraft.block.Blocks;
+import vazkii.quark.base.handler.VariantHandler;
+import vazkii.quark.base.module.LoadModule;
+import vazkii.quark.base.module.Module;
+import vazkii.quark.base.module.ModuleCategory;
+import vazkii.quark.building.block.VerticalSlabBlock;
+
+@LoadModule(category = ModuleCategory.BUILDING)
+public class VerticalSlabsModule extends Module {
+
+	@Override
+	public void modulesStarted() {
+		ImmutableSet.of(Blocks.ACACIA_SLAB, Blocks.ANDESITE_SLAB, Blocks.BIRCH_SLAB, Blocks.BRICK_SLAB, Blocks.COBBLESTONE_SLAB, 
+				Blocks.CUT_RED_SANDSTONE_SLAB, Blocks.CUT_SANDSTONE_SLAB, Blocks.DARK_OAK_SLAB, Blocks.DARK_PRISMARINE_SLAB, Blocks.DIORITE_SLAB, 
+				Blocks.END_STONE_BRICK_SLAB, Blocks.GRANITE_SLAB, Blocks.JUNGLE_SLAB, Blocks.MOSSY_COBBLESTONE_SLAB, Blocks.MOSSY_STONE_BRICK_SLAB, 
+				Blocks.NETHER_BRICK_SLAB, Blocks.OAK_SLAB, Blocks.POLISHED_ANDESITE_SLAB, Blocks.POLISHED_DIORITE_SLAB, Blocks.POLISHED_GRANITE_SLAB, 
+				Blocks.PRISMARINE_SLAB, Blocks.PRISMARINE_BRICK_SLAB, Blocks.PURPUR_SLAB, Blocks.QUARTZ_SLAB, Blocks.RED_NETHER_BRICK_SLAB, 
+				Blocks.RED_SANDSTONE_SLAB, Blocks.SANDSTONE_SLAB, Blocks.SMOOTH_QUARTZ_SLAB, Blocks.SMOOTH_RED_SANDSTONE_SLAB, Blocks.SMOOTH_SANDSTONE_SLAB, 
+				Blocks.SMOOTH_STONE_SLAB, Blocks.SPRUCE_SLAB, Blocks.STONE_SLAB, Blocks.STONE_BRICK_SLAB)
+		.forEach(b -> new VerticalSlabBlock(b, this));
+		
+		VariantHandler.SLABS.forEach(b -> new VerticalSlabBlock(b, this));
+	}
+	
+}
