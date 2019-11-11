d165c62c0978e088657bfab2b8d745c0ba479039
Vasco Lavos <vazkii@hotmail.com>
2019-11-11 08:52:35 +0000

    Crabs now check material for spwaning. Closes #1660

--- src/main/java/vazkii/quark/world/entity/CrabEntity.java
+++ src/main/java/vazkii/quark/world/entity/CrabEntity.java
@@ -10,15 +10,35 @@
  */
 package vazkii.quark.world.entity;
 
+import java.util.Random;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import com.google.common.collect.Lists;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
-import net.minecraft.entity.*;
+import net.minecraft.entity.AgeableEntity;
+import net.minecraft.entity.CreatureAttribute;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntitySize;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Pose;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.AttributeModifier.Operation;
-import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.goal.BreedGoal;
+import net.minecraft.entity.ai.goal.FollowParentGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.PanicGoal;
+import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
 import net.minecraft.entity.effect.LightningBoltEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -50,10 +70,6 @@ import vazkii.quark.base.handler.QuarkSounds;
 import vazkii.quark.world.ai.RaveGoal;
 import vazkii.quark.world.module.PassiveCreaturesModule;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Random;
-
 public class CrabEntity extends AnimalEntity implements IEntityAdditionalSpawnData {
 
 	public static final ResourceLocation CRAB_LOOT_TABLE = new ResourceLocation("quark", "entities/crab");
@@ -81,7 +97,7 @@ public class CrabEntity extends AnimalEntity implements IEntityAdditionalSpawnDa
 	}
 
 	public static boolean spawnPredicate(EntityType<? extends AnimalEntity> type, IWorld world, SpawnReason reason, BlockPos pos, Random random) {
-		return world.getBlockState(pos.down()).getBlock() == Blocks.SAND && world.getLightSubtracted(pos, 0) > 8;
+		return world.getBlockState(pos.down()).getMaterial() == Material.SAND && world.getLightSubtracted(pos, 0) > 8;
 	}
 
 	public static void rave(IWorld world, BlockPos pos, boolean raving) {
