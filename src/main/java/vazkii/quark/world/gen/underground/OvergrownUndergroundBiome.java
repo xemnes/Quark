519880f5100c6ff6ee73bf63ca0a2709eba2448c
Vasco Lavos <vazkii@hotmail.com>
2019-12-03 18:46:47 +0000

    Fix wood pillars spawning in the surface. Closes #1675

--- src/main/java/vazkii/quark/world/gen/underground/OvergrownUndergroundBiome.java
+++ src/main/java/vazkii/quark/world/gen/underground/OvergrownUndergroundBiome.java
@@ -14,31 +14,28 @@ public class OvergrownUndergroundBiome extends BasicUndergroundBiome {
 	}
 
 	@Override
-	public void finalCeilingPass(Context context, BlockPos pos) {
+	public void fillFloor(Context context, BlockPos pos, BlockState state) {
+		if(context.random.nextBoolean())
+			context.world.setBlockState(pos, Blocks.COARSE_DIRT.getDefaultState(), 2);
+		else super.fillFloor(context, pos, state);
+		
 		IWorld world = context.world;
 		if(context.random.nextDouble() < 0.025) {
 			int count = 0;
 			for(int i = 0; i < 20; i++) {
-				BlockPos checkPos = pos.add(0, -i, 0);
-				if(isFloor(world, checkPos, world.getBlockState(checkPos))) {
+				BlockPos checkPos = pos.add(0, i, 0);
+				if(isCeiling(world, checkPos, world.getBlockState(checkPos))) {
 					count = i;
 					break;
 				}
 			}
 			
 			for(int i = 0; i <= count; i++) {
-				BlockPos placePos = pos.add(0, -i, 0);
+				BlockPos placePos = pos.add(0, i, 0);
 				world.setBlockState(placePos, Blocks.OAK_LOG.getDefaultState(), 2);
 			}
 			
 		}
-	}
-	
-	@Override
-	public void fillFloor(Context context, BlockPos pos, BlockState state) {
-		if(context.random.nextBoolean())
-			context.world.setBlockState(pos, Blocks.COARSE_DIRT.getDefaultState(), 2);
-		else super.fillFloor(context, pos, state);
 	}
 
 }
