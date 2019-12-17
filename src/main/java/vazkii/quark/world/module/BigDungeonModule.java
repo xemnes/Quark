80aa96c1733a50884666854fd0f0e4c947278624
Vasco Lavos <vazkii@hotmail.com>
2019-12-17 18:48:43 +0000

    Fixed mega dungeons spwaning in the wrong places. Closes #1731

--- src/main/java/vazkii/quark/world/module/BigDungeonModule.java
+++ src/main/java/vazkii/quark/world/module/BigDungeonModule.java
@@ -7,13 +7,14 @@ import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.Structure;
 import net.minecraft.world.gen.placement.IPlacementConfig;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.common.BiomeDictionary.Type;
 import net.minecraftforge.registries.ForgeRegistries;
 import vazkii.arl.util.RegistryHelper;
 import vazkii.quark.base.module.Config;
 import vazkii.quark.base.module.LoadModule;
 import vazkii.quark.base.module.Module;
 import vazkii.quark.base.module.ModuleCategory;
-import vazkii.quark.experimental.debug.FloodFillItem;
+import vazkii.quark.base.world.config.BiomeTypeConfig;
 import vazkii.quark.world.gen.structure.BigDungeonStructure;
 
 @LoadModule(category = ModuleCategory.WORLD)
@@ -33,6 +34,9 @@ public class BigDungeonModule extends Module {
 	@Config
 	public static double chestChance = 0.5;
 	
+	@Config
+	public static BiomeTypeConfig biomeTypes = new BiomeTypeConfig(true, Type.OCEAN, Type.BEACH, Type.NETHER, Type.END);
+	
 	@Override
 	public void construct() {
 //		new FloodFillItem(this);
@@ -45,8 +49,10 @@ public class BigDungeonModule extends Module {
 	public void setup() {
 		if(enabled)
 			for(Biome b : ForgeRegistries.BIOMES.getValues()) {
-				b.addStructure(structure, NoFeatureConfig.NO_FEATURE_CONFIG);
-				b.addFeature(Decoration.UNDERGROUND_STRUCTURES, Biome.createDecoratedFeature(structure, IFeatureConfig.NO_FEATURE_CONFIG, Placement.NOPE, IPlacementConfig.NO_PLACEMENT_CONFIG));
+				if(biomeTypes.canSpawn(b)) {
+					b.addStructure(structure, NoFeatureConfig.NO_FEATURE_CONFIG);
+					b.addFeature(Decoration.UNDERGROUND_STRUCTURES, Biome.createDecoratedFeature(structure, IFeatureConfig.NO_FEATURE_CONFIG, Placement.NOPE, IPlacementConfig.NO_PLACEMENT_CONFIG));	
+				}
 			}
 	}
 
