b9c16b1c977faab97d796512a17ba0e589335393
Vasco Lavos <vazkii@hotmail.com>
2019-12-29 17:54:31 +0000

    Allow elytra boost configuring

--- src/main/java/vazkii/quark/tweaks/module/CampfiresBoostElytraModule.java
+++ src/main/java/vazkii/quark/tweaks/module/CampfiresBoostElytraModule.java
@@ -9,38 +9,41 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import vazkii.quark.base.module.Config;
 import vazkii.quark.base.module.LoadModule;
 import vazkii.quark.base.module.Module;
 import vazkii.quark.base.module.ModuleCategory;
 
 @LoadModule(category = ModuleCategory.TWEAKS, hasSubscriptions = true)
 public class CampfiresBoostElytraModule extends Module {
+	
+	@Config public double boostStrength = 0.5;
+	@Config public double maxSpeed = 1;
 
 	@SubscribeEvent
 	public void onPlayerTick(PlayerTickEvent event) {
 		PlayerEntity player = event.player;
 		
 		if(player.isElytraFlying()) {
-			BlockPos pos = player.getPosition();
-			World world = player.world;
-			
-			int moves = 0;
-			while(world.isAirBlock(pos) && pos.getY() > 0 && moves < 20) {
-				pos = pos.down();
-				moves++;
-			}
-			
-			BlockState state = world.getBlockState(pos);
-			if(state.getBlock() == Blocks.CAMPFIRE && state.get(CampfireBlock.LIT) && state.get(CampfireBlock.SIGNAL_FIRE)) {
-				double force = 0.5;
-				if(moves > 16)
-					force -= (force * (1.0 - ((double) moves - 16.0) / 4.0));
+			Vec3d motion = player.getMotion();
+			if(motion.getY() < maxSpeed) {
+				BlockPos pos = player.getPosition();
+				World world = player.world;
 				
-				double cap = 1.0;
+				int moves = 0;
+				while(world.isAirBlock(pos) && pos.getY() > 0 && moves < 20) {
+					pos = pos.down();
+					moves++;
+				}
 				
-				Vec3d motion = player.getMotion();
-				double trueCap = Math.max(motion.getY(), cap);
-				player.setMotion(motion.getX(), Math.min(trueCap, motion.getY() + force), motion.getZ());
+				BlockState state = world.getBlockState(pos);
+				if(state.getBlock() == Blocks.CAMPFIRE && state.get(CampfireBlock.LIT) && state.get(CampfireBlock.SIGNAL_FIRE)) {
+					double force = boostStrength;
+					if(moves > 16)
+						force -= (force * (1.0 - ((double) moves - 16.0) / 4.0));
+					
+					player.setMotion(motion.getX(), Math.min(maxSpeed, motion.getY() + force), motion.getZ());
+				}
 			}
 		}
 	}
