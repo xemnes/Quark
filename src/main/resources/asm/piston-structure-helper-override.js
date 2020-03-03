6e7447cb2b68df71821a05debf0fbb19b1c6db17
yrsegal@gmail.com <yrsegal@gmail.com>
2020-03-03 10:20:45 -0500

    this is why vazkii's not allowed to asm

--- src/main/resources/asm/piston-structure-helper-override.js
+++ src/main/resources/asm/piston-structure-helper-override.js
@@ -1,3 +1,33 @@
+function injectForEachMethod(method, targetType, clazz, targetName, sig, callback) {
+    var ASM = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+
+    var target = ASM.findFirstMethodCall(method,
+        targetType,
+        clazz,
+        targetName,
+        sig);
+
+    while (target !== null) {
+        var index = method.instructions.indexOf(target);
+        var indexShift = callback(target, index);
+
+        var newIndex = method.instructions.indexOf(target);
+        if (newIndex !== -1)
+            index = newIndex;
+        else if (typeof indexShift === 'number')
+            index += indexShift;
+
+        target = ASM.findFirstMethodCallAfter(method,
+            targetType,
+            clazz,
+            targetName,
+            sig,
+            index + 1);
+    }
+
+    return method;
+}
+
 function initializeCoreMod() {
     return {
         'do-move-replace-structure-helper': {
@@ -13,27 +43,26 @@ function initializeCoreMod() {
                 var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
                 var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
 
-                var target = ASM.findFirstMethodCall(method, 
-                    ASM.MethodType.SPECIAL, 
+                return injectForEachMethod(method, ASM.MethodType.SPECIAL,
                     "net/minecraft/block/state/PistonBlockStructureHelper",
                     "<init>",
-                    "(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;Z)V");
-                
-                var newInstructions = new InsnList();
-                newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 1));
-                newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 2));
-                newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 3));
-                newInstructions.add(new VarInsnNode(Opcodes.ILOAD, 4));
-                newInstructions.add(ASM.buildMethodCall(
-                    "vazkii/quark/base/handler/AsmHooks", 
-                    "transformStructureHelper",
-                    "(Lnet/minecraft/block/state/PistonBlockStructureHelper;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;Z)Lnet/minecraft/block/state/PistonBlockStructureHelper;", 
-                    ASM.MethodType.STATIC
-                ));
+                    "(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;Z)V",
+                    function (target) {
 
-                method.instructions.insert(target, newInstructions);
+                        var newInstructions = new InsnList();
+                        newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 1));
+                        newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 2));
+                        newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 3));
+                        newInstructions.add(new VarInsnNode(Opcodes.ILOAD, 4));
+                        newInstructions.add(ASM.buildMethodCall(
+                            "vazkii/quark/base/handler/AsmHooks",
+                            "transformStructureHelper",
+                            "(Lnet/minecraft/block/state/PistonBlockStructureHelper;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;Z)Lnet/minecraft/block/state/PistonBlockStructureHelper;",
+                            ASM.MethodType.STATIC
+                        ));
 
-                return method;
+                        method.instructions.insert(target, newInstructions);
+                    });
             }
         },
 
@@ -51,27 +80,28 @@ function initializeCoreMod() {
                 var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
                 var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
 
-                var target = ASM.findFirstMethodCall(method, 
+                return injectForEachMethod(method,
                     ASM.MethodType.SPECIAL, 
                     "net/minecraft/block/state/PistonBlockStructureHelper",
                     "<init>",
-                    "(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;Z)V");
-                
-                var newInstructions = new InsnList();
-                newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 1));
-                newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 2));
-                newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 4));
-                newInstructions.add(new InsnNode(Opcodes.ICONST_1));
-                newInstructions.add(ASM.buildMethodCall(
-                    "vazkii/quark/base/handler/AsmHooks", 
-                    "transformStructureHelper",
-                    "(Lnet/minecraft/block/state/PistonBlockStructureHelper;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;Z)Lnet/minecraft/block/state/PistonBlockStructureHelper;", 
-                    ASM.MethodType.STATIC
-                ));
+                    "(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;Z)V",
+                function (target) {
 
-                method.instructions.insert(target, newInstructions);
+                    var newInstructions = new InsnList();
+                    newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 1));
+                    newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 2));
+                    newInstructions.add(new VarInsnNode(Opcodes.ALOAD, 4));
+                    newInstructions.add(new InsnNode(Opcodes.ICONST_1));
+                    newInstructions.add(ASM.buildMethodCall(
+                        "vazkii/quark/base/handler/AsmHooks",
+                        "transformStructureHelper",
+                        "(Lnet/minecraft/block/state/PistonBlockStructureHelper;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/Direction;Z)Lnet/minecraft/block/state/PistonBlockStructureHelper;",
+                        ASM.MethodType.STATIC
+                    ));
 
-                return method;
+                    method.instructions.insert(target, newInstructions);
+
+                });
             }
         }
     }
