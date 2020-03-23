5044d55c575e9e82c0c521880e39fa4d3f222399
Vasco Lavos <vazkii@hotmail.com>
2020-03-23 16:14:11 +0000

    Added iron farm killswitch

--- src/main/java/vazkii/quark/mobs/module/ToretoiseModule.java
+++ src/main/java/vazkii/quark/mobs/module/ToretoiseModule.java
@@ -3,10 +3,13 @@ package vazkii.quark.mobs.module;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EntitySpawnPlacementRegistry.PlacementType;
+import net.minecraft.item.Items;
 import net.minecraft.world.gen.Heightmap.Type;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import vazkii.arl.util.RegistryHelper;
 import vazkii.quark.base.module.Config;
@@ -21,13 +24,13 @@ import vazkii.quark.mobs.client.render.ToretoiseRenderer;
 import vazkii.quark.mobs.entity.StonelingEntity;
 import vazkii.quark.mobs.entity.ToretoiseEntity;
 
-@LoadModule(category = ModuleCategory.MOBS)
+@LoadModule(category = ModuleCategory.MOBS, hasSubscriptions = true)
 public class ToretoiseModule extends Module {
 
 	public static EntityType<ToretoiseEntity> toretoiseType;
 	
-	@Config
-	public static int maxYLevel = 32;
+	@Config public static boolean disableIronFarms = false;
+	@Config public static int maxYLevel = 32;
 	
 	@Config
 	public static DimensionConfig dimensions = DimensionConfig.overworld(false);
@@ -54,6 +57,12 @@ public class ToretoiseModule extends Module {
 	@OnlyIn(Dist.CLIENT)
 	public void clientSetup() {
 		RenderingRegistry.registerEntityRenderingHandler(toretoiseType, ToretoiseRenderer::new);
+	}
+	
+	@SubscribeEvent
+	public void onLoot(LivingDropsEvent event) {
+		if(disableIronFarms && event.getEntity().getType() == EntityType.IRON_GOLEM)
+			event.getDrops().removeIf(e -> e.getItem().getItem() == Items.IRON_INGOT);
 	}
 	
 }
