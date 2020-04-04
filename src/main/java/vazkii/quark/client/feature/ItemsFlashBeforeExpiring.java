d448912371bcb5af301f88703eec9247a656e7f4
yrsegal@gmail.com <yrsegal@gmail.com>
2020-04-04 17:12:24 -0400

    Fix #1966

--- src/main/java/vazkii/quark/client/feature/ItemsFlashBeforeExpiring.java
+++ src/main/java/vazkii/quark/client/feature/ItemsFlashBeforeExpiring.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import vazkii.arl.network.NetworkHandler;
@@ -53,7 +52,8 @@ public class ItemsFlashBeforeExpiring extends Feature {
 
 			if (age != prev && age != prev + 1) {
 				anyChange = true;
-			}
+			} else
+				AGE_MAP.put(item, prev + 1);
 		}
 
 		if (!LIFESPAN_MAP.containsKey(item))
