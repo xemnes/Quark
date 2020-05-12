d6b997f70ba6817c9bddfd3894a5bf6028741cb7
Vasco Lavos <vazkii@hotmail.com>
2020-05-12 16:07:26 +0100

    Fix #2129

--- src/main/java/vazkii/quark/mobs/module/FoxhoundModule.java
+++ src/main/java/vazkii/quark/mobs/module/FoxhoundModule.java
@@ -2,11 +2,14 @@ package vazkii.quark.mobs.module;
 
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntitySpawnPlacementRegistry.PlacementType;
+import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.BiomeDictionary.Type;
+import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import vazkii.arl.util.RegistryHelper;
 import vazkii.quark.base.module.Config;
@@ -25,34 +28,43 @@ import vazkii.quark.mobs.entity.FoxhoundEntity;
  */
 @LoadModule(category = ModuleCategory.MOBS, hasSubscriptions = true)
 public class FoxhoundModule extends Module {
-	
-    public static EntityType<FoxhoundEntity> foxhoundType;
 
-    @Config(description = "The chance coal will tame a foxhound")
-    public static double tameChance = 0.05;
-    
-    @Config
-    public static EntitySpawnConfig spawnConfig = new EntitySpawnConfig(30, 1, 2, new BiomeTypeConfig(false, Type.NETHER));
+	public static EntityType<FoxhoundEntity> foxhoundType;
 
-    @Override
-    public void construct() {
-        foxhoundType = EntityType.Builder.create(FoxhoundEntity::new, EntityClassification.CREATURE)
-                .size(0.8F, 0.8F)
-                .setTrackingRange(80)
-                .setUpdateInterval(3)
-                .setShouldReceiveVelocityUpdates(true)
-                .immuneToFire()
-                .setCustomClientFactory((spawnEntity, world) -> new FoxhoundEntity(foxhoundType, world))
-                .build("foxhound");
-        RegistryHelper.register(foxhoundType, "foxhound");
-        
-        EntitySpawnHandler.registerSpawn(this, foxhoundType, EntityClassification.MONSTER, PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, FoxhoundEntity::spawnPredicate, spawnConfig);
-        EntitySpawnHandler.addEgg(foxhoundType, 0x890d0d, 0xf2af4b, spawnConfig);
-    }
+	@Config(description = "The chance coal will tame a foxhound")
+	public static double tameChance = 0.05;
 
-    @Override
-    @OnlyIn(Dist.CLIENT)
-    public void clientSetup() {
-        RenderingRegistry.registerEntityRenderingHandler(foxhoundType, FoxhoundRenderer::new);
-    }
+	@Config
+	public static EntitySpawnConfig spawnConfig = new EntitySpawnConfig(30, 1, 2, new BiomeTypeConfig(false, Type.NETHER));
+
+	@Override
+	public void construct() {
+		foxhoundType = EntityType.Builder.create(FoxhoundEntity::new, EntityClassification.CREATURE)
+				.size(0.8F, 0.8F)
+				.setTrackingRange(80)
+				.setUpdateInterval(3)
+				.setShouldReceiveVelocityUpdates(true)
+				.immuneToFire()
+				.setCustomClientFactory((spawnEntity, world) -> new FoxhoundEntity(foxhoundType, world))
+				.build("foxhound");
+		RegistryHelper.register(foxhoundType, "foxhound");
+
+		EntitySpawnHandler.registerSpawn(this, foxhoundType, EntityClassification.MONSTER, PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, FoxhoundEntity::spawnPredicate, spawnConfig);
+		EntitySpawnHandler.addEgg(foxhoundType, 0x890d0d, 0xf2af4b, spawnConfig);
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientSetup() {
+		RenderingRegistry.registerEntityRenderingHandler(foxhoundType, FoxhoundRenderer::new);
+	}
+
+	@SubscribeEvent
+	public void onAggro(LivingSetAttackTargetEvent event) {
+		if(event.getTarget() != null 
+				&& event.getEntityLiving().getType() == EntityType.IRON_GOLEM 
+				&& event.getTarget().getType() == foxhoundType 
+				&& ((FoxhoundEntity) event.getTarget()).isTamed())
+			((IronGolemEntity) event.getEntityLiving()).setAttackTarget(null);
+	}
 }
