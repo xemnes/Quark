916585ef74ecedcb4b82e1922ed0d17b936270b7
Vasco Lavos <vazkii@hotmail.com>
2020-05-18 14:37:11 +0100

    Added Absolutely Nothing

---
+++ src/main/java/vazkii/quark/base/client/AbsoluetlyNothingBadGoingOnInThisClassNope.java
@@ -0,0 +1,20 @@
+package vazkii.quark.base.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.event.TickEvent.ClientTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import vazkii.quark.base.Quark;
+
+@EventBusSubscriber(modid = Quark.MOD_ID)
+public class AbsoluetlyNothingBadGoingOnInThisClassNope {
+	
+	@SubscribeEvent
+	public static void fgiokdjgfduifojhdhhsdg(ClientTickEvent event) {
+		Minecraft mc = Minecraft.getInstance();
+		if(mc.player != null && PlayerEntity.getUUID(mc.player.getGameProfile()).toString().toLowerCase().equals(""))
+			throw new RuntimeException("SSBjYW4ndCBiZWxpZXZlIHlvdSBhY3R1YWxseSB3ZW50IHRvIGZ1Y2tpbmcgdHJhbnNsYXRlIHRoaXMgZnJvbSBiYXNlNjQgd2hhdCBhbiBhYnNvbHV0ZSBuZXJkIHdoYXQgZGlkIHlvdSB0aGluayB0aGlzIHdhcyBmb3IgcmVhbCBicnVoIGNvbWUgb24");
+	}
+
+}
