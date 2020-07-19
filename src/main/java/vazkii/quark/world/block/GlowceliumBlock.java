8a3621bad647a4b2b0e3e174d598a35420acfe2c
Vasco Lavos <vazkii@hotmail.com>
2020-07-19 13:08:44 +0100

    Glowcelium can now hold everything mycellium can.

--- src/main/java/vazkii/quark/world/block/GlowceliumBlock.java
+++ src/main/java/vazkii/quark/world/block/GlowceliumBlock.java
@@ -13,12 +13,15 @@ import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.lighting.LightEngine;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.PlantType;
 import net.minecraftforge.common.ToolType;
 import vazkii.quark.base.block.QuarkBlock;
 import vazkii.quark.base.module.Module;
@@ -70,6 +73,11 @@ public class GlowceliumBlock extends QuarkBlock {
 
 		if(rand.nextInt(40) == 0)
 			worldIn.addParticle(ParticleTypes.END_ROD, pos.getX() + rand.nextDouble(), pos.getY() + 1.15, pos.getZ() + rand.nextDouble(), 0, 0, 0);
+	}
+	
+	@Override
+	public boolean canSustainPlant(BlockState state, IBlockReader world, BlockPos pos, Direction facing, IPlantable plantable) {
+		return Blocks.MYCELIUM.canSustainPlant(state, world, pos, facing, plantable);
 	}
 
 }
